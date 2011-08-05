fb2e8becdaf5ccc5d24550db840420f21dea9724
Jon Schindler <jschindl@brainvis.wustl.edu>
2011-08-05 16:26:26 -0500

    Converted windows build system to JOM so that it can take advantage of multiple cores.

--- build_scripts/windows32/build.bat
+++ build_scripts/windows32/build.bat
@@ -4,7 +4,7 @@ if [%MACHINE%]==[] call setup_env.bat
 set OLD_DIR=%CD%
 md %DIR%\caret7_source\build
 cd /d %DIR%\caret7_source\build
-cmake -G "NMake Makefiles" ../src
-nmake 
+cmake -G "NMake Makefiles JOM" ../src
+jom -j8
 cd /d %OLD_DIR%
 set OLD_DIR=
