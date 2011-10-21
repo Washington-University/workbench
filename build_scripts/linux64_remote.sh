684601c04d162a7c29376119041613d4a1a0314c
Jon Schindler <jschindl@brainvis.wustl.edu>
2011-10-21 14:41:23 -0500

    Changed build location back to default, since QT can't be moved without being rebuilt.  Added line to sumsdev fstab to mount hippocampus to /miniserver, like it has in the past.

--- build_scripts/linux64_remote.sh
+++ build_scripts/linux64_remote.sh
@@ -2,5 +2,5 @@
 BUILD=linux64
 BUILD_SERVER=sumsdev.wustl.edu
 
-BUILD_DIR=/mnt/miniserver/caret7_development/${BUILD}
+BUILD_DIR=/miniserver/caret7_development/${BUILD}
 ssh -v caret@${BUILD_SERVER} ${BUILD_DIR}/caret7_source/build_scripts/${BUILD}.sh $1 > $PWD/remote_launch_${BUILD}.txt 2>&1
