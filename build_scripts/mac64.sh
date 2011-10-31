cfed6cc3fa8becc65ac0f54026b9205ea9d05e85
Caret <john@brainmap.wustl.edu>
2011-10-31 14:47:54 -0500

    Working on mac64 build script

--- build_scripts/mac64.sh
+++ build_scripts/mac64.sh
@@ -8,86 +8,24 @@
 # (2) Build Caret programs
 # (3) Copy executables into the distribution
 #
-# Based off Tim Coalson's "buildbot" script
-#
-
-#
-# Go to correct directory
-#
-BUILD_ROOT_DIR=/Volumes/DS4600/caret7_development/mac64
-GIT_ROOT_DIR=${BUILD_ROOT_DIR}/caret7_source
-SRC_DIR=${GIT_ROOT_DIR}/src
-BUILD_DIR=${BUILD_ROOT_DIR}/build
-cd ${BUILD_ROOT_DIR}
-
-#
-# Setup Environment
-#
-
-#for now we use caret5's lib dir
-#LIB_DIR=/Volumes/DS4600/caret_development/mac64
-#QTDIR=${LIB_DIR}/libraries/qt-software/qt-4.7.1-static-64bit
-
-#
-# Use Qt SDK
-#
-QTDIR=/Users/caret/QtSDK/Desktop/Qt/474/gcc
-export QTDIR
-
-PATH=${QTDIR}/bin:${PATH}
-export PATH
-
-#
-# Go into git checkout directory
-#
-cd ${GIT_ROOT_DIR}
 
 #
 # File for capturing standard error
 #
+BUILD_ROOT_DIR=/Volumes/DS4600/caret7_development/mac64
 ERROR_FILE=${BUILD_ROOT_DIR}/result_build_mac64.txt
 rm -f ${ERROR_FILE}
-echo "Caret7 Mac 64 Build Result" > ${ERROR_FILE}
 
 #
-# Update source from repository
+# If something fails, keep going
 #
-git pull -u >> ${ERROR_FILE} 2>&1
-#hg update >> ${ERROR_FILE} 2>&1
+set noon
 
 #
-# Force updating __DATE__ that shows up in title bar
+# Run the build script
 #
-#if [ "$1" == "FORCE_UPDATE" ]
-#then
-#echo "Update Forced" >> ${ERROR_FILE} 2>&1
-#rm -f caret/GuiMainWindow.o caret/caret5.app caret_command/caret_command.app caret_edit/caret_edit.app caret_command_operations/CommandVersion.o caret_edit/main.o >> ${ERROR_FILE} 2>&1
-#fi
-
-#
-# Configure and build as release.
-# Build twice so that if the build fails, the second build will
-# contain just the output that shows the errors.
-# Catch output and echo to screen.
-#
-mkdir -p ${BUILD_DIR} >> ${ERROR_FILE} 2>&1
-cd ${BUILD_DIR} >> ${ERROR_FILE} 2>&1
-cmake ${SRC_DIR} >> ${ERROR_FILE} 2>&1
-make -j2 
-make -j2 >> ${ERROR_FILE} 2>&1
-cat ${ERROR_FILE}
-
-#
-# Run 'macdeployqt' on the App so that frameworks are copied
-#
-macdeployqt Desktop/workbench.app >> ${ERROR_FILE} 2>&1
-
-#
-# Copy to distribution directory
-#
-DIST_DIR=/Volumes/DS4600/caret7_distribution/caret/macosx64_apps
-/bin/cp -rv Desktop/workbench.app ${DIST_DIR} >> ${ERROR_FILE} 2>&1
-/bin/cp -v CommandLine/caret_command ${DIST_DIR} >> ${ERROR_FILE} 2>&1
+echo "Starting mac64 aux script" >> ${ERROR_FILE} 2>&1
+${BUILD_ROOT_DIR}/caret7_source/build_scripts/mac64_aux.sh >> ${ERROR_FILE} 2>&1
 
 #
 # Send output as email
