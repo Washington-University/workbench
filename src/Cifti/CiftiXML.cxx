79a43e1d2f9a5698192ed16aa404aac22afa6cab
Tim Coalson <tsc5yc@mst.edu>
2011-11-01 15:22:16 -0500

    initial (untested!) implementation of convenience functions in CiftiXML

--- src/Cifti/CiftiXML.cxx
+++ src/Cifti/CiftiXML.cxx
@@ -22,3 +22,341 @@
  *
  */
 /*LICENSE_END*/
+
+#include "CiftiXML.h"
+#include "CiftiFileException.h"
+
+using namespace caret;
+using namespace std;
+
+int64_t CiftiXML::getSurfaceIndex(const int64_t node, const CiftiBrainModelElement* myElement, const int64_t numContig) const
+{
+    if (myElement == NULL) return -1;
+    if (node < numContig)//NOTE: this is how an unspecified node list of size of the entire surface gets mapped!
+    {
+        return myElement->m_indexOffset + node;
+    }
+    int64_t numIndices = (int64_t)myElement->m_nodeIndices.size();
+    for (int64_t i = numContig; i < numIndices; ++i)
+    {
+        if (myElement->m_nodeIndices[i] == node)
+        {
+            return myElement->m_indexOffset + i;
+        }
+    }
+    return -1;
+}
+
+int64_t CiftiXML::getColumnIndexForNode(const int64_t node, const StructureEnum::Enum structure) const
+{
+    if (m_colMap == NULL || m_colMap->m_indicesMapToDataType != CIFTI_INDEX_TYPE_BRAIN_MODELS)
+    {
+        return -1;
+    }
+    bool left = false;
+    switch (structure)
+    {
+    case StructureEnum::CORTEX_LEFT:
+        left = true;
+        break;
+    case StructureEnum::CORTEX_RIGHT:
+        break;
+    default:
+        return -1;
+    };
+    if (left)
+    {
+        return getSurfaceIndex(node, m_colLeftSurfModel, m_colLeftSurfContig);
+    } else {
+        return getSurfaceIndex(node, m_colRightSurfModel, m_colRightSurfContig);
+    }
+}
+
+int64_t CiftiXML::getRowIndexForNode(const int64_t node, const caret::StructureEnum::Enum structure) const
+{
+    if (m_rowMap == NULL || m_rowMap->m_indicesMapToDataType != CIFTI_INDEX_TYPE_BRAIN_MODELS)
+    {
+        return -1;
+    }
+    bool left = false;
+    switch (structure)
+    {
+    case StructureEnum::CORTEX_LEFT:
+        left = true;
+        break;
+    case StructureEnum::CORTEX_RIGHT:
+        break;
+    default:
+        return -1;
+    };
+    if (left)
+    {
+        return getSurfaceIndex(node, m_rowLeftSurfModel, m_rowLeftSurfContig);
+    } else {
+        return getSurfaceIndex(node, m_rowRightSurfModel, m_rowRightSurfContig);
+    }
+}
+
+int64_t CiftiXML::getVolumeIndex(const int64_t* ijk, const caret::CiftiMatrixIndicesMapElement* myMap) const
+{
+    if (myMap == NULL || myMap->m_indicesMapToDataType != CIFTI_INDEX_TYPE_BRAIN_MODELS)
+    {
+        return -1;
+    }
+    for (int64_t i = 0; i < (int64_t)myMap->m_brainModels.size(); ++i)
+    {
+        if (myMap->m_brainModels[i].m_modelType == CIFTI_MODEL_TYPE_VOXELS)
+        {
+            const vector<voxelIndexType>& myVoxels = myMap->m_brainModels[i].m_voxelIndicesIJK;
+            int64_t voxelArraySize = (int64_t)myVoxels.size();
+            for (int64_t j = 0; j < voxelArraySize; j += 3)
+            {
+                if ((ijk[0] == (int64_t)myVoxels[j]) && (ijk[1] == (int64_t)myVoxels[j + 1]) && (ijk[2] == (int64_t)myVoxels[j + 2]))
+                {
+                    return j / 3;
+                }
+            }
+        }
+    }
+}
+
+int64_t CiftiXML::getColumnIndexForVoxel(const int64_t* ijk) const
+{
+    return getVolumeIndex(ijk, m_colMap);
+}
+
+int64_t CiftiXML::getRowIndexForVoxel(const int64_t* ijk) const
+{
+    return getVolumeIndex(ijk, m_rowMap);
+}
+
+bool CiftiXML::getSurfaceMapping(vector<CiftiSurfaceMap>& mappingOut, CiftiBrainModelElement* myModel, const int64_t numContig) const
+{
+    if (myModel == NULL) return false;
+    int64_t mappingSize = (int64_t)myModel->m_indexCount;
+    mappingOut.resize(mappingSize);
+    for (int64_t i = 0; i < mappingSize; ++i)
+    {
+        mappingOut[i].m_ciftiIndex = myModel->m_indexOffset + i;
+        if (i < numContig)//NOTE: this is how an unspecified node list of size of the entire surface gets mapped!
+        {
+            mappingOut[i].m_surfaceNode = i;
+        } else {
+            mappingOut[i].m_surfaceNode = myModel->m_nodeIndices[i];
+        }
+    }
+}
+
+bool CiftiXML::getSurfaceMapForColumns(vector<CiftiSurfaceMap>& mappingOut, const StructureEnum::Enum structure) const
+{
+    mappingOut.clear();
+    bool left = false;
+    switch (structure)
+    {
+    case StructureEnum::CORTEX_LEFT:
+        left = true;
+        break;
+    case StructureEnum::CORTEX_RIGHT:
+        break;
+    default:
+        return false;
+    };
+    if (left)
+    {
+        return getSurfaceMapping(mappingOut, m_colLeftSurfModel, m_colLeftSurfContig);
+    } else {
+        return getSurfaceMapping(mappingOut, m_colRightSurfModel, m_colRightSurfContig);
+    }
+}
+
+bool CiftiXML::getSurfaceMapForRows(vector<CiftiSurfaceMap>& mappingOut, const StructureEnum::Enum structure) const
+{
+    mappingOut.clear();
+    bool left = false;
+    switch (structure)
+    {
+    case StructureEnum::CORTEX_LEFT:
+        left = true;
+        break;
+    case StructureEnum::CORTEX_RIGHT:
+        break;
+    default:
+        return false;
+    };
+    if (left)
+    {
+        return getSurfaceMapping(mappingOut, m_rowLeftSurfModel, m_rowLeftSurfContig);
+    } else {
+        return getSurfaceMapping(mappingOut, m_rowRightSurfModel, m_colRightSurfContig);
+    }
+}
+
+bool CiftiXML::getVolumeMapping(vector<CiftiVolumeMap>& mappingOut, CiftiMatrixIndicesMapElement* myMap, int64_t myCount) const
+{
+    if (myMap == NULL || myMap->m_indicesMapToDataType != CIFTI_INDEX_TYPE_BRAIN_MODELS)
+    {
+        return false;
+    }
+    mappingOut.resize(myCount);
+    int64_t myIndex = 0;
+    for (int64_t i = 0; i < (int64_t)myMap->m_brainModels.size(); ++i)
+    {
+        if (myMap->m_brainModels[i].m_modelType == CIFTI_MODEL_TYPE_VOXELS)
+        {
+            vector<voxelIndexType>& myVoxels = myMap->m_brainModels[i].m_voxelIndicesIJK;
+            int64_t voxelArraySize = (int64_t)myVoxels.size();
+            int64_t modelOffset = myMap->m_brainModels[i].m_indexOffset;
+            int64_t j1 = 0;
+            for (int64_t j = 0; j < voxelArraySize; j += 3)
+            {
+                mappingOut[myIndex].m_ciftiIndex = modelOffset + j1;
+                mappingOut[myIndex].m_ijk[0] = myVoxels[j];
+                mappingOut[myIndex].m_ijk[1] = myVoxels[j + 1];
+                mappingOut[myIndex].m_ijk[2] = myVoxels[j + 2];
+                ++j1;
+                ++myIndex;
+            }
+        }
+    }
+}
+
+bool CiftiXML::getVolumeMapForColumns(vector<CiftiVolumeMap>& mappingOut, const caret::StructureEnum::Enum structure) const
+{
+    mappingOut.clear();
+    return getVolumeMapping(mappingOut, m_colMap, m_colVoxels);
+}
+
+bool CiftiXML::getVolumeMapForRows(vector< CiftiVolumeMap >& mappingOut, const caret::StructureEnum::Enum structure) const
+{
+    mappingOut.clear();
+    return getVolumeMapping(mappingOut, m_rowMap, m_rowVoxels);
+}
+
+void CiftiXML::rootChanged()
+{//and here is where the real work is done
+    m_colMap = NULL;//first, invalidate everything
+    m_colLeftSurfModel = NULL;
+    m_colRightSurfModel = NULL;
+    m_colLeftSurfNodes = 0;
+    m_colRightSurfNodes = 0;
+    m_colLeftSurfContig = 0;
+    m_colRightSurfContig = 0;
+    m_colVoxels = 0;
+    m_rowMap = NULL;
+    m_rowLeftSurfModel = NULL;
+    m_rowRightSurfModel = NULL;
+    m_rowLeftSurfNodes = 0;
+    m_rowRightSurfNodes = 0;
+    m_rowLeftSurfContig = 0;
+    m_rowRightSurfContig = 0;
+    m_rowVoxels = 0;
+    CiftiMatrixElement& myMatrix = m_root.m_matrices[0];//assume only one matrix
+    int numMaps = (int)myMatrix.m_matrixIndicesMap.size();
+    for (int i = 0; i < numMaps; ++i)
+    {
+        if (myMatrix.m_matrixIndicesMap[i].m_indicesMapToDataType == CIFTI_INDEX_TYPE_BRAIN_MODELS)
+        {
+            CiftiMatrixIndicesMapElement& myMap = myMatrix.m_matrixIndicesMap[i];
+            int numDimensions = (int)myMap.m_appliesToMatrixDimension.size();
+            for (int j = 0; j < numDimensions; ++j)
+            {
+                if (myMap.m_appliesToMatrixDimension[j] == 0)
+                {
+                    if (m_rowMap != NULL)
+                    {
+                        throw CiftiFileException("Multiple mappings on the same dimension not supported");
+                    }
+                    m_rowMap = &myMap;
+                    int numModels = (int)myMap.m_brainModels.size();//over 2 billion models? unlikely
+                    for (int k = 0; k < numModels; ++k)
+                    {
+                        if (myMap.m_brainModels[k].m_modelType == CIFTI_MODEL_TYPE_SURFACE)
+                        {
+                            int64_t thisContig = myMap.m_brainModels[k].m_indexCount;//if the loop interior never executes (empty list) it is a special case that all are contiguous
+                            for (int64_t m = 0; m < myMap.m_brainModels[k].m_indexCount; ++m)//similarly, if the loop never encounters not equals, all are correct
+                            {
+                                if (m != myMap.m_brainModels[k].m_nodeIndices[m])
+                                {
+                                    thisContig = m;
+                                    break;
+                                }
+                            }
+                            if (myMap.m_brainModels[k].m_brainStructure.endsWith("_LEFT"))
+                            {
+                                if (m_rowLeftSurfModel != NULL)
+                                {
+                                    throw CiftiFileException("Multiple surfaces of the same handedness not supported");
+                                }
+                                m_rowLeftSurfModel = &(myMap.m_brainModels[k]);
+                                m_rowLeftSurfNodes = myMap.m_brainModels[k].m_surfaceNumberOfNodes;
+                                m_rowLeftSurfContig = thisContig;
+                            }
+                            if (myMap.m_brainModels[k].m_brainStructure.endsWith("_RIGHT"))
+                            {
+                                if (m_rowRightSurfModel != NULL)
+                                {
+                                    throw CiftiFileException("Multiple surfaces of the same handedness not supported");
+                                }
+                                m_rowRightSurfModel = &(myMap.m_brainModels[k]);
+                                m_rowRightSurfNodes = myMap.m_brainModels[k].m_surfaceNumberOfNodes;
+                                m_rowRightSurfContig = thisContig;
+                            }
+                        }
+                        if (myMap.m_brainModels[k].m_modelType == CIFTI_MODEL_TYPE_VOXELS)
+                        {
+                            m_rowVoxels += myMap.m_brainModels[k].m_indexCount;
+                        }
+                    }
+                }
+                if (myMap.m_appliesToMatrixDimension[j] == 1)
+                {
+                    if (m_colMap != NULL)
+                    {
+                        throw CiftiFileException("Multiple mappings on the same dimension not supported");
+                    }
+                    m_colMap = &myMap;
+                    int numModels = (int)myMap.m_brainModels.size();//over 2 billion models? unlikely
+                    for (int k = 0; k < numModels; ++k)
+                    {
+                        if (myMap.m_brainModels[k].m_modelType == CIFTI_MODEL_TYPE_SURFACE)
+                        {
+                            int64_t thisContig = myMap.m_brainModels[k].m_indexCount;//if the loop interior never executes (empty list) it is a special case that all are contiguous
+                            for (int64_t m = 0; m < myMap.m_brainModels[k].m_indexCount; ++m)//similarly, if the loop never encounters not equals, all are correct
+                            {
+                                if (m != myMap.m_brainModels[k].m_nodeIndices[m])
+                                {
+                                    thisContig = m;
+                                    break;
+                                }
+                            }
+                            if (myMap.m_brainModels[k].m_brainStructure.endsWith("_LEFT"))
+                            {
+                                if (m_colLeftSurfModel != NULL)
+                                {
+                                    throw CiftiFileException("Multiple surfaces of the same handedness not supported");
+                                }
+                                m_colLeftSurfModel = &(myMap.m_brainModels[k]);
+                                m_colLeftSurfNodes = myMap.m_brainModels[k].m_surfaceNumberOfNodes;
+                                m_colLeftSurfContig = thisContig;
+                            }
+                            if (myMap.m_brainModels[k].m_brainStructure.endsWith("_RIGHT"))
+                            {
+                                if (m_colRightSurfModel != NULL)
+                                {
+                                    throw CiftiFileException("Multiple surfaces of the same handedness not supported");
+                                }
+                                m_colRightSurfModel = &(myMap.m_brainModels[k]);
+                                m_colRightSurfNodes = myMap.m_brainModels[k].m_surfaceNumberOfNodes;
+                                m_colRightSurfContig = thisContig;
+                            }
+                        }
+                        if (myMap.m_brainModels[k].m_modelType == CIFTI_MODEL_TYPE_VOXELS)
+                        {
+                            m_colVoxels += myMap.m_brainModels[k].m_indexCount;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- src/Cifti/CiftiXML.h
+++ src/Cifti/CiftiXML.h
@@ -24,6 +24,7 @@
 /*LICENSE_END*/
 #ifndef __CIFTI_XML
 #define __CIFTI_XML
+#include "StructureEnum.h"
 #include "CiftiXMLElements.h"
 #include "CiftiXMLReader.h"
 #include "CiftiXMLWriter.h"
@@ -31,100 +32,153 @@
 
 namespace caret {
 /// Simple Container class for storing Cifti XML meta data
-class CiftiXML {
-public:
-    //TODO create initializers for various types of XML meta data (Dense Connectivity, Dense Time Series, etc)
-    /**
-    * Default Constructor
-    *
-    * Default Constructor
-    */
-    CiftiXML() { }
-    /**
-    * Constructor
-    *
-    * Constructor, create class using already existing Cifti xml tree
-    * @param xml_root
-    */
-    CiftiXML(CiftiRootElement &xml_root) { m_root = xml_root; }
-    /**
-    * Constructor
-    *
-    * Constructor, create class using ASCII formatted byte array that
-    * containes xml meta data text
-    * @param bytes
-    */
-    CiftiXML(const QByteArray &bytes) { readXML(bytes); }
-    /**
-    * Constructor
-    *
-    * Constructor, create class using QString that contains xml
-    * meta data text
-    * @param xml_string
-    */
-    CiftiXML(const QString &xml_string) { readXML(xml_string); }
-    /**
-    * Constructor
-    *
-    * Constructor, create class using QXmlStreamReader.
-    * QXmlStreamReader is assumed to be reading from Cifti XML
-    * Text.
-    * @param xml_stream
-    */
-    CiftiXML(QXmlStreamReader &xml_stream) { readXML(xml_stream); }
-    /**
-    * readXML
-    *
-    * readXML, replacing the currently Cifti XML Root, if it exists
-    * @param bytes an ASCII formatted byte array that contains Cifti XML data
-    */
-    void readXML(const QByteArray &bytes) { QString text(bytes);readXML(text);}
-    /**
-    * readXML
-    *
-    * readXML, replacing the currently Cifti XML Root, if it exists
-    * @param text QString that contains Cifti XML data
-    */
-    void readXML(const QString &text) {QXmlStreamReader xml(text); readXML(xml);}
-    /**
-    * readXML
-    *
-    * readXML, replacing the currently Cifti XML Root, if it exists
-    * @param xml_stream
-    */
-    void readXML(QXmlStreamReader &xml_stream) { parseCiftiXML(xml_stream,m_root);}
-    /**
-    * writeXML
-    *
-    * write the Cifti XML data to the supplied QString
-    * @param text
-    */
-    void writeXML(QString &text) { QXmlStreamWriter xml(&text); writeCiftiXML(xml,m_root);}
-    /**
-    * writeXML
-    *
-    * write the Cifti XML data to the supplied byte array.
-    * @param bytes
-    */
-    void writeXML(QByteArray &bytes) { QXmlStreamWriter xml(&bytes); writeCiftiXML(xml,m_root);}
+    struct CiftiSurfaceMap
+    {
+        int64_t m_ciftiIndex;
+        int64_t m_surfaceNode;
+    };
+    
+    struct CiftiVolumeMap
+    {
+        int64_t m_ciftiIndex;
+        int64_t m_ijk[3];
+    };
+    
+    class CiftiXML {
+    public:
+        //TODO create initializers for various types of XML meta data (Dense Connectivity, Dense Time Series, etc)
+        /**
+        * Default Constructor
+        *
+        * Default Constructor
+        */
+        CiftiXML() { }
+        /**
+        * Constructor
+        *
+        * Constructor, create class using already existing Cifti xml tree
+        * @param xml_root
+        */
+        CiftiXML(CiftiRootElement &xml_root) { m_root = xml_root; rootChanged(); }
+        /**
+        * Constructor
+        *
+        * Constructor, create class using ASCII formatted byte array that
+        * containes xml meta data text
+        * @param bytes
+        */
+        CiftiXML(const QByteArray &bytes) { readXML(bytes); }
+        /**
+        * Constructor
+        *
+        * Constructor, create class using QString that contains xml
+        * meta data text
+        * @param xml_string
+        */
+        CiftiXML(const QString &xml_string) { readXML(xml_string); }
+        /**
+        * Constructor
+        *
+        * Constructor, create class using QXmlStreamReader.
+        * QXmlStreamReader is assumed to be reading from Cifti XML
+        * Text.
+        * @param xml_stream
+        */
+        CiftiXML(QXmlStreamReader &xml_stream) { readXML(xml_stream); }
+        /**
+        * readXML
+        *
+        * readXML, replacing the currently Cifti XML Root, if it exists
+        * @param bytes an ASCII formatted byte array that contains Cifti XML data
+        */
+        void readXML(const QByteArray &bytes) { QString text(bytes);readXML(text);}
+        /**
+        * readXML
+        *
+        * readXML, replacing the currently Cifti XML Root, if it exists
+        * @param text QString that contains Cifti XML data
+        */
+        void readXML(const QString &text) {QXmlStreamReader xml(text); readXML(xml);}
+        /**
+        * readXML
+        *
+        * readXML, replacing the currently Cifti XML Root, if it exists
+        * @param xml_stream
+        */
+        void readXML(QXmlStreamReader &xml_stream) { parseCiftiXML(xml_stream,m_root); rootChanged(); }
+        /**
+        * writeXML
+        *
+        * write the Cifti XML data to the supplied QString
+        * @param text
+        */
+        void writeXML(QString &text) { QXmlStreamWriter xml(&text); writeCiftiXML(xml,m_root);}
+        /**
+        * writeXML
+        *
+        * write the Cifti XML data to the supplied byte array.
+        * @param bytes
+        */
+        void writeXML(QByteArray &bytes) { QXmlStreamWriter xml(&bytes); writeCiftiXML(xml,m_root);}
 
-    /**
-    * setXMLRoot
-    *
-    * set the Cifti XML root
-    * @param xml_root
-    */
-    void setXMLRoot (CiftiRootElement &xml_root) { m_root = xml_root; }
-    /**
-    * getXMLRoot
-    *
-    * get a copy of the Cifti XML Root
-    * @param xml_root
-    */
-    void getXMLRoot (CiftiRootElement &xml_root) { xml_root = m_root; }
-protected:
-    CiftiRootElement m_root;
-};
+        /**
+        * setXMLRoot
+        *
+        * set the Cifti XML root
+        * @param xml_root
+        */
+        void setXMLRoot (CiftiRootElement &xml_root) { m_root = xml_root; rootChanged(); }
+        /**
+        * getXMLRoot
+        *
+        * get a copy of the Cifti XML Root
+        * @param xml_root
+        */
+        void getXMLRoot (CiftiRootElement &xml_root) { xml_root = m_root; }
+        
+        ///get the row index for a node, returns -1 if it doesn't find a matching mapping
+        int64_t getRowIndexForNode(const int64_t node, const StructureEnum::Enum structure) const;
+        
+        ///get the column index for a node, returns -1 if it doesn't find a matching mapping
+        int64_t getColumnIndexForNode(const int64_t node, const StructureEnum::Enum structure) const;
+        
+        ///SLOW! - get the row index for a voxel, returns -1 if it doesn't find a matching mapping
+        int64_t getRowIndexForVoxel(const int64_t* ijk) const;
+        
+        ///SLOW! - get the column index for a voxel, returns -1 if it doesn't find a matching mapping
+        int64_t getColumnIndexForVoxel(const int64_t* ijk) const;
+        
+        ///get the mapping for a surface in rows, returns false and empty vector if not found
+        bool getSurfaceMapForRows(std::vector<CiftiSurfaceMap>& mappingOut, const StructureEnum::Enum structure) const;
+        
+        ///get the mapping for a surface in columns, returns false and empty vector if not found
+        bool getSurfaceMapForColumns(std::vector<CiftiSurfaceMap>& mappingOut, const StructureEnum::Enum structure) const;
+            
+        ///get the mapping for a surface in rows, returns false and empty vector if not found
+        bool getVolumeMapForRows(std::vector<CiftiVolumeMap>& mappingOut, const StructureEnum::Enum structure) const;
+        
+        ///get the mapping for a surface in columns, returns false and empty vector if not found
+        bool getVolumeMapForColumns(std::vector<CiftiVolumeMap>& mappingOut, const StructureEnum::Enum structure) const;
+            
+    protected:
+        CiftiRootElement m_root;
+        CiftiMatrixIndicesMapElement* m_rowMap, *m_colMap;//assumes only one matrix
+        CiftiBrainModelElement* m_rowLeftSurfModel, *m_rowRightSurfModel, *m_colLeftSurfModel, *m_colRightSurfModel;
+        int64_t m_rowLeftSurfContig, m_rowRightSurfContig, m_colLeftSurfContig, m_colRightSurfContig;//shortcut for surface mappings that are trivial (or partly trivial)
+        int64_t m_rowVoxels, m_colVoxels;
+        int64_t m_rowLeftSurfNodes, m_rowRightSurfNodes, m_colLeftSurfNodes, m_colRightSurfNodes;
+        
+        ///updates the member variables associated with our root
+        void rootChanged();
+        
+        ///some boilerplate to get the correct index in a particular mapping
+        int64_t getSurfaceIndex(const int64_t node, const CiftiBrainModelElement* myModel, const int64_t numContig) const;
+        int64_t getVolumeIndex(const int64_t* ijk, const CiftiMatrixIndicesMapElement* myMap) const;
+        
+        ///some boilerplate to build mappings
+        bool getSurfaceMapping(std::vector<CiftiSurfaceMap>& mappingOut, CiftiBrainModelElement* myModel, const int64_t numContig) const;
+        bool getVolumeMapping(std::vector<CiftiVolumeMap>& mappingOut, CiftiMatrixIndicesMapElement* myMap, const int64_t myCount) const;
+    };
 
 }
 #endif//__CIFTI_XML
