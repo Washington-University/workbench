03dcc0be69a4739784413378cd61e5b1e3513bc3
John Harwell <john@brainmap.wustl.edu>
2011-11-02 08:16:02 -0500

    Updating mac32 build scripts

--- build_scripts/mac32.sh
+++ build_scripts/mac32.sh
@@ -8,79 +8,32 @@
 # (2) Build Caret programs
 # (3) Copy executables into the distribution
 #
-# Based off Tim Coalson's "buildbot" script
-#
-
-#
-# Go to correct directory
-#
-BUILD_ROOT_DIR=/Volumes/DS4600/caret7_development/mac32
-GIT_ROOT_DIR=${BUILD_ROOT_DIR}/caret7_source
-SRC_DIR=${GIT_ROOT_DIR}/src
-BUILD_DIR=${BUILD_ROOT_DIR}/build
-cd ${BUILD_ROOT_DIR}
-
-#
-# Setup Environment
-#
-
-#for now we use caret5's lib dir
-LIB_DIR=/Volumes/DS4600/caret_development/mac32
-QTDIR=${LIB_DIR}/libraries/qt-software/qt-4.7.1-static-32bit
-export QTDIR
-
-PATH=${QTDIR}/bin:${PATH}
-export PATH
-
-#
-# Go into git checkout directory
-#
-cd ${GIT_ROOT_DIR}
 
 #
 # File for capturing standard error
 #
+BUILD_ROOT_DIR=/Volumes/DS4600/caret7_development/mac32
 ERROR_FILE=${BUILD_ROOT_DIR}/result_build_mac32.txt
 rm -f ${ERROR_FILE}
-echo "Caret7 Mac 32 Build Result" > ${ERROR_FILE}
 
 #
-# Update source from repository
+# If something fails, keep going
 #
-git pull -u >> ${ERROR_FILE} 2>&1
-#hg update >> ${ERROR_FILE} 2>&1
+set noon
 
 #
-# Force updating __DATE__ that shows up in title bar
+# Run the build script
 #
-#if [ "$1" == "FORCE_UPDATE" ]
-#then
-#echo "Update Forced" >> ${ERROR_FILE} 2>&1
-#rm -rfv caret/GuiMainWindow.o caret/caret5.app caret_command/caret_command.app caret_edit/caret_edit.app caret_command_operations/CommandVersion.o caret_edit/main.o >> ${ERROR_FILE} 2>&1
-#fi
-
-#
-# Configure and build as release.
-# Build twice so that if the build fails, the second build will
-# contain just the output that shows the errors.
-# Catch output and echo to screen.
-#
-mkdir -p ${BUILD_DIR} >> ${ERROR_FILE} 2>&1
-cd ${BUILD_DIR} >> ${ERROR_FILE} 2>&1
-cmake -DCMAKE_OSX_ARCHITECTURES=i386 ${SRC_DIR} >> ${ERROR_FILE} 2>&1
-make -j2 
-make -j2 >> ${ERROR_FILE} 2>&1
-cat ${ERROR_FILE}
-
-#
-# Copy to distribution directory
-#
-DIST_DIR=/Volumes/DS4600/caret7_distribution/caret/macosx32_apps
-/bin/cp -rv Desktop/workbench.app ${DIST_DIR} >> ${ERROR_FILE} 2>&1
-/bin/cp -v CommandLine/caret_command ${DIST_DIR} >> ${ERROR_FILE} 2>&1
+echo "Starting mac32 aux script" >> ${ERROR_FILE} 2>&1
+${BUILD_ROOT_DIR}/caret7_source/build_scripts/mac32_aux.sh >> ${ERROR_FILE} 2>&1
+echo "Finished mac32 aux script" >> ${ERROR_FILE} 2>&1
 
 #
 # Send output as email
 #
-cat ${ERROR_FILE} | mail -s 'Caret7 Mac 32 Build Result' john@brainvis.wustl.edu jschindl@brainvis.wustl.edu tsc5yc@mst.edu
+MAIL_ERROR_FILE=${BUILD_ROOT_DIR}/mail_result_build_mac32.txt
+rm -f ${MAIL_ERROR_FILE}
+cat ${ERROR_FILE} | mail -v -s 'Caret7 Mac 32 Build Result' john@brainvis.wustl.edu jschindl@brainvis.wustl.edu tsc5yc@mst.edu >> ${MAIL_ERROR_FILE} 2>&1
+
+echo "Sent mail"
 
