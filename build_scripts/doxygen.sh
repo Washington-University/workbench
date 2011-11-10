ae95581849d3d8794422fbb3d7f6cd705fa7b2ae
John Harwell <john@brainmap.wustl.edu>
2011-11-10 09:06:21 -0600

    Adding scripts for generation of doxygen documentation

---
+++ build_scripts/doxygen.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+#
+# This file is executed by (via "launchd")
+#    /System/Library/LaunchDaemons/edu.wustl.caret7.doxygen.plist
+#
+
+#
+# File for capturing standard error
+#
+BUILD_ROOT_DIR=/Volumes/DS4600/caret7_development/doxygen
+ERROR_FILE=${BUILD_ROOT_DIR}/result_doxygen.txt
+rm -f ${ERROR_FILE}
+
+#
+# If something fails, keep going
+#
+set noon
+
+#
+# Run the build script
+#
+echo "Starting doxygen generation" >> ${ERROR_FILE} 2>&1
+${BUILD_ROOT_DIR}/caret7_source/build_scripts/doxygen_aux.sh >> ${ERROR_FILE} 2>&1
+echo "Finished doxygen generation" >> ${ERROR_FILE} 2>&1
+
+#
+# Send output as email
+#
+cat ${ERROR_FILE} | mail -v -s 'Doxygen Generation for Caret7' john@brainvis.wustl.edu jschindl@brainvis.wustl.edu tsc5yc@mst.edu
+
+echo "Sent mail"
+
---
+++ build_scripts/doxygen_aux.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+#
+# This file is executed by (via "launchd")
+#    /System/Library/LaunchDaemons/edu.wustl.caret7.doxygen.plist
+#
+# (1) Update source code from repository
+# (2) Build doxygen documentation
+#
+
+#
+# Go to correct directory
+#
+MAIN_DIR=/Volumes/DS4600/caret7_development/doxygen
+SOURCE_DIR=${MAIN_DIR}/caret7_source
+DOCUMENTATION_DIR=${MAIN_DIR}/documentation
+BUILD_DIR=${MAIN_DIR}/build
+
+#
+# Remove existing documentation directory
+#
+rm -rf ${DOCUMENTATION_DIR}
+
+#
+# Go to the source directory which contains Doxyfile
+# Doxyfile is read by doxygen and contains configuration information
+#
+cd ${SOURCE_DIR}
+
+#
+# Update source from repository
+#
+echo "UPDATING SOURCE FROM GIT REPOSITORY"
+git pull -u
+
+#
+# Generate doxygen documentation
+#
+echo "Generating Documentation"
+doxygen
+
+echo "SCRIPT COMPLETED SUCCESSFULLY"
+
