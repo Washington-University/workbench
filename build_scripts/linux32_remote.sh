d76eca605888f0b5095dc12f995035ff37a6270a
Jon Schindler <jschindl@brainvis.wustl.edu>
2011-11-23 13:41:53 -0600

    Fixed typo in subject.

--- build_scripts/linux32_remote.sh
+++ build_scripts/linux32_remote.sh
@@ -4,4 +4,4 @@ BUILD_SERVER=128.252.37.32
 
 BUILD_DIR=/home/caret/${BUILD}/caret7_dev
 ssh -v caret@${BUILD_SERVER} "/bin/bash -c \"cd ${BUILD_DIR};./build32.sh $1\"" > $PWD/remote_launch_${BUILD}.txt 2>&1
-cat $PWD/remote_launch_${BUILD}.txt | mail -s 'Caret7 Linux32 Build Result' john@brainvis.wustl.edu jschindl@brainvis.wustl.edu tsc5yc@mst.edu
+cat $PWD/remote_launch_${BUILD}.txt | mail -s 'Caret7 Linux 32 Build Result' john@brainvis.wustl.edu jschindl@brainvis.wustl.edu tsc5yc@mst.edu
