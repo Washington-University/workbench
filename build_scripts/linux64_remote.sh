644ea209bdb5ecfb27033c5abebdbc8b55a7c15e
Jon Schindler <jschindl@brainvis.wustl.edu>
2011-11-23 13:17:05 -0600

    Modified build scripts to run build on chroot environmehnt on my workstation jonsdev/128.252.37.32

--- build_scripts/linux64_remote.sh
+++ build_scripts/linux64_remote.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 BUILD=linux64
-BUILD_SERVER=sumsdev.wustl.edu
+BUILD_SERVER=128.252.37.32
 
-BUILD_DIR=/miniserver/caret7_development/${BUILD}
-ssh -v caret@${BUILD_SERVER} ${BUILD_DIR}/caret7_source/build_scripts/${BUILD}.sh $1 > $PWD/remote_launch_${BUILD}.txt 2>&1
+BUILD_DIR=/home/caret/${BUILD}/caret7_dev
+ssh -v caret@${BUILD_SERVER} "/bin/bash -c \"cd ${BUILD_DIR};./build64.sh $1\"" > $PWD/remote_launch_${BUILD}.txt 2>&1
+cat $PWD/remote_launch_${BUILD}.txt | mail -s 'Caret7 Linux64 Build Result' john@brainvis.wustl.edu jschindl@brainvis.wustl.edu tsc5yc@mst.edu
+
