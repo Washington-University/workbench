5e3eaefe3ce514899a1d40d620f22e9602efa78b
John Harwell <john@brainmap.wustl.edu>
2012-01-04 16:37:30 -0600

    Added SurfaceProjectionTypeEnum which is an enumerated type for the three types of surface projections, unprojected, vanessen, and barycentric

---
+++ src/Files/SurfaceProjectionTypeEnum.h
@@ -0,0 +1,119 @@
+#ifndef __SURFACE_PROJECTION_TYPE_ENUM__H_
+#define __SURFACE_PROJECTION_TYPE_ENUM__H_
+
+/*LICENSE_START*/
+/*
+ * Copyright 2012 Washington University,
+ * All rights reserved.
+ *
+ * Connectome DB and Connectome Workbench are part of the integrated Connectome 
+ * Informatics Platform.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    * Neither the names of Washington University nor the
+ *      names of its contributors may be used to endorse or promote products
+ *      derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+/*LICENSE_END*/
+
+
+#include <stdint.h>
+#include <vector>
+#include "AString.h"
+
+namespace caret {
+
+class SurfaceProjectionTypeEnum {
+
+public:
+    /**
+     * Enumerated values.
+     */
+    enum Enum {
+        /** Barycentric Projection */
+        BARYCENTRIC,
+        /** Van Essen Projection */
+        VANESSEN,
+        /** Not Projected */
+        UNPROJECTED
+    };
+
+
+    ~SurfaceProjectionTypeEnum();
+
+    static AString toName(Enum enumValue);
+    
+    static Enum fromName(const AString& name, bool* isValidOut);
+    
+    static AString toGuiName(Enum enumValue);
+    
+    static Enum fromGuiName(const AString& guiName, bool* isValidOut);
+    
+    static int32_t toIntegerCode(Enum enumValue);
+    
+    static Enum fromIntegerCode(const int32_t integerCode, bool* isValidOut);
+
+    static void getAllEnums(std::vector<Enum>& allEnums);
+
+    static void getAllNames(std::vector<AString>& allNames, const bool isSorted);
+
+    static void getAllGuiNames(std::vector<AString>& allGuiNames, const bool isSorted);
+
+private:
+    SurfaceProjectionTypeEnum(const Enum enumValue, 
+                 const AString& name,
+                 const AString& guiName);
+
+    static const SurfaceProjectionTypeEnum* findData(const Enum enumValue);
+
+    /** Holds all instance of enum values and associated metadata */
+    static std::vector<SurfaceProjectionTypeEnum> enumData;
+
+    /** Initialize instances that contain the enum values and metadata */
+    static void initialize();
+
+    /** Indicates instance of enum values and metadata have been initialized */
+    static bool initializedFlag;
+    
+    /** Auto generated integer codes */
+    static int32_t integerCodeCounter;
+    
+    /** The enumerated type value for an instance */
+    Enum enumValue;
+
+    /** The integer code associated with an enumerated value */
+    int32_t integerCode;
+
+    /** The name, a text string that is identical to the enumerated value */
+    AString name;
+    
+    /** A user-friendly name that is displayed in the GUI */
+    AString guiName;
+};
+
+#ifdef __SURFACE_PROJECTION_TYPE_ENUM_DECLARE__
+std::vector<SurfaceProjectionTypeEnum> SurfaceProjectionTypeEnum::enumData;
+bool SurfaceProjectionTypeEnum::initializedFlag = false;
+int32_t SurfaceProjectionTypeEnum::integerCodeCounter = 0; 
+#endif // __SURFACE_PROJECTION_TYPE_ENUM_DECLARE__
+
+} // namespace
+#endif  //__SURFACE_PROJECTION_TYPE_ENUM__H_
