c2c5824299946a0b7ba42e5112bf649d463d9d62
Jon Schindler <jschindl@brainvis.wustl.edu>
2012-01-12 13:17:36 -0600

    Time Course Dialog can now render plots.

--- src/GuiQt/TimeCourseDialog.cxx
+++ src/GuiQt/TimeCourseDialog.cxx
@@ -27,11 +27,52 @@
 #include "TimeCourseDialog.h"
 #include "ui_TimeCourseDialog.h"
 
+//qwt includes
+#include <qapplication.h>
+#include "qlayout.h"
+#include "qwt_plot.h"
+#include "qwt_plot_marker.h"
+#include "qwt_plot_curve.h"
+#include "qwt_legend.h"
+#include "qwt_series_data.h"
+#include "qwt_plot_canvas.h"
+#include "qwt_plot_panner.h"
+#include "qwt_plot_magnifier.h"
+#include "qwt_text.h"
+#include "qwt_math.h"
+#include "math.h"
+
+
+
 TimeCourseDialog::TimeCourseDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::TimeCourseDialog)
 {
     ui->setupUi(this);
+
+    PlotTC *plot = new PlotTC();
+
+
+
+
+    // We put a dummy widget around to have
+    // so that Qt paints a widget background
+    // when resizing
+    std::vector<double>x;
+    std::vector<double>y;
+    for(int64_t i=0;i<5;i++)
+    {
+        x.push_back((double)i);
+        y.push_back((double)i);
+    }
+
+
+    plot->populate(x,y);
+    ui->verticalLayout_4->setContentsMargins(0,0,0,0);
+    ui->verticalLayout_4->insertWidget(0,plot,100);
+    //plot->setGeometry(0,0,5,5);
+    //plot->update();
+
 }
 
 TimeCourseDialog::~TimeCourseDialog()
@@ -40,4 +81,105 @@ TimeCourseDialog::~TimeCourseDialog()
 }
 
 
+
+
+void TimeCourseDialog::on_TDClose_clicked()
+{
+    this->hide();
+}
+
+void TimeCourseDialog::on_TDClearChart_clicked()
+{
+
+}
+
+void TimeCourseDialog::on_TDMinTime_valueChanged(double arg1)
+{
+
+}
+
+void TimeCourseDialog::on_TDMaxTime_valueChanged(double arg1)
+{
+
+}
+
+void TimeCourseDialog::on_TDMinActivity_valueChanged(double arg1)
+{
+
+}
+
+void TimeCourseDialog::on_TDMaxActivity_valueChanged(double arg1)
+{
+
+}
+
+void TimeCourseDialog::on_TDShowAverage_stateChanged(int arg1)
+{
+
+}
+
+void TimeCourseDialog::on_TDShowAverage_toggled(bool checked)
+{
+
+}
+
+void TimeCourseDialog::on_TDKeepLast_valueChanged(double arg1)
+{
+
+}
+
+PlotTC::PlotTC(QWidget *parent):
+    QwtPlot( parent )
+{
+    // panning with the left mouse button
+    (void) new QwtPlotPanner( canvas() );
+
+    // zoom in/out with the wheel
+    (void) new QwtPlotMagnifier( canvas() );
+
+    setAutoFillBackground( true );
+    setPalette( QPalette( QColor( 165, 193, 228 ) ) );    
+
+    setTitle("Time Course");
+    insertLegend(new QwtLegend(), QwtPlot::RightLegend);
+
+    // canvas
+    canvas()->setLineWidth( 1 );
+    canvas()->setFrameStyle( QFrame::Box | QFrame::Plain );
+    canvas()->setBorderRadius( 1 );//15
+
+    QPalette canvasPalette( Qt::white );
+    canvasPalette.setColor( QPalette::Foreground, QColor( 133, 190, 232 ) );
+    canvas()->setPalette( canvasPalette );
+
+
+}
+
+void PlotTC::populate(std::vector<double> &x, std::vector<double> &y)
+{
+    // Insert new curves
+    //setAxisScale(xBottom,0,x.size()-1);
+    //setAxisScale(yLeft,0,y.size()-1);
+
+    QwtPlotCurve *tc = new QwtPlotCurve();
+    tc->setRenderHint(QwtPlotItem::RenderAntialiased);
+    //tc->setLegendAttribute(QwtPlotCurve::LegendShowLine, true);
+    tc->setPen(QPen(Qt::red));
+
+
+    QVector<double> qx;
+    QVector<double> qy;
+    qx = qx.fromStdVector(x);
+    qy = qy.fromStdVector(y);
+
+    tc->setSamples(qx,qy);
+    tc->attach(this);
+
+
+}
+
+void PlotTC::resizeEvent( QResizeEvent *event )
+{
+    QwtPlot::resizeEvent( event );
+}
 
--- src/GuiQt/TimeCourseDialog.h
+++ src/GuiQt/TimeCourseDialog.h
@@ -1,3 +1,5 @@
+#ifndef __TIME_COURSE_DIALOG__
+#define __TIME_COURSE_DIALOG__
 /*LICENSE_START*/
 /* 
  *  Copyright 1995-2011 Washington University School of Medicine 
@@ -25,13 +27,23 @@
 
 #include <QWidget>
 #include "AString.h"
-//qwt includes
-
-
 
 #include <QDialog>
-#ifndef __TIME_COURSE_DIALOG__
-#define __TIME_COURSE_DIALOG__
+//qwt includes
+#include <qapplication.h>
+#include "qlayout.h"
+#include "qwt_plot.h"
+#include "qwt_plot_marker.h"
+#include "qwt_plot_curve.h"
+#include "qwt_legend.h"
+#include "qwt_series_data.h"
+#include "qwt_plot_canvas.h"
+#include "qwt_plot_panner.h"
+#include "qwt_plot_magnifier.h"
+#include "qwt_text.h"
+#include "qwt_math.h"
+#include "math.h"
+
 
 namespace Ui {
     class TimeCourseDialog;
@@ -45,8 +57,72 @@ public:
     explicit TimeCourseDialog(QWidget *parent = 0);
     ~TimeCourseDialog();
 
+private slots:
+    void on_TDClose_clicked();
+
+    void on_TDClearChart_clicked();
+
+    void on_TDMinTime_valueChanged(double arg1);
+
+    void on_TDMaxTime_valueChanged(double arg1);
+
+    void on_TDMinActivity_valueChanged(double arg1);
+
+    void on_TDMaxActivity_valueChanged(double arg1);
+
+    void on_TDShowAverage_stateChanged(int arg1);
+
+    void on_TDShowAverage_toggled(bool checked);
+
+    void on_TDKeepLast_valueChanged(double arg1);
+
 private:
     Ui::TimeCourseDialog *ui;
 };
+
+
+
+//-----------------------------------------------------------------
+//              simple.cpp
+//
+//      A simple example which shows how to use QwtPlot connected
+//      to a data class without any storage, calculating each values
+//      on the fly.
+//-----------------------------------------------------------------
+
+class PlotTC : public QwtPlot
+{
+public:
+    PlotTC( QWidget *parent = NULL);
+     void populate(std::vector<double> &x, std::vector<double> &y);
+
+protected:
+    virtual void resizeEvent( QResizeEvent * );
+
+};
+
+
+/*
+int main(int argc, char **argv)
+{
+    QApplication a(argc, argv);
+
+    Plot *plot = new Plot();
+
+    // We put a dummy widget around to have
+    // so that Qt paints a widget background
+    // when resizing
+
+    QWidget window;
+    QHBoxLayout *layout = new QHBoxLayout( &window );
+    layout->setContentsMargins( 0, 0, 0, 0 );
+    layout->addWidget( plot );
+
+    window.resize(600,400);
+    window.show();
+
+    return a.exec();
+}*/
+
 
 #endif //__TIME_COURSE_DIALOG__
