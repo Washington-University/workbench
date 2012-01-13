849237fc0b70a7e2ac4ec25ad5543b667b4c7697
Tim Coalson <tsc5yc@mst.edu>
2012-01-13 17:14:57 -0600

    Initial command line options framework for workbench

--- src/Desktop/desktop.cxx
+++ src/Desktop/desktop.cxx
@@ -31,7 +31,9 @@
 #include <QThread>
 
 #include <cstdlib>
+#include <iostream>
 
+#include "CaretAssert.h"
 #include "CaretHttpManager.h"
 #include "CaretLogger.h"
 #include "EventBrowserWindowNew.h"
@@ -41,10 +43,12 @@
 #include "SessionManager.h"
 #include "SystemUtilities.h"
 #include "WuQtUtilities.h"
+#include "FileInformation.h"
 
 static bool caretLoggerIsValid = false;
 
 using namespace caret;
+using namespace std;
 
 /*
  * From http://developer.qt.nokia.com/wiki/How_to_create_a_splash_screen_with_an_induced_delay
@@ -123,135 +127,258 @@ messageHandlerForQt(QtMsgType type, const char* msg)
     }
 }
 
+//struct for communicating stuff back to main from parseCommandLine
+struct ProgramState
+{
+    vector<AString> fileList;
+    int specLoadType;
+    ProgramState();
+};
+
+//declare the functions associated with command line
+void printHelp(const AString& progName);
+void parseCommandLine(const AString& progName, ProgramParameters* myParams, ProgramState& myState);
+
 int 
 main(int argc, char* argv[])
 {
-    /*
-     * Handle uncaught exceptions
-     */
-    SystemUtilities::setHandlersForUnexpected();
-    
-    qInstallMsgHandler(messageHandlerForQt);
-    QApplication app(argc, argv);
-    QApplication::setApplicationName("Connectome Workbench");
-    QApplication::setApplicationVersion("0");
-    QApplication::setOrganizationDomain("brainvis.wustl.edu");
-    QApplication::setOrganizationName("Van Essen Lab");
-    
-    /*
-     * Splash Screen
-     */
-    QPixmap splashPixmap;
-    QSplashScreen splashScreen;
-    if (WuQtUtilities::loadPixmap(":/splash_hcp.png", splashPixmap)) {
-        splashScreen.setPixmap(splashPixmap);
-        splashScreen.showMessage("Starting Workbench...");
-        splashScreen.show();
-        app.processEvents();
-        Sleeper::sleep(2);
-    }
-    
-    /*
-     * Make sure OpenGL is available.
-     */
-    if (!QGLFormat::hasOpenGL()) { 
-        qWarning( "This system has no OpenGL support. Exiting." );
-        return -1;
-    }
-        
-    /*
-     * Setup OpenGL
-     */
-    QGLFormat glfmt;
-    glfmt.setRedBufferSize(8);
-    glfmt.setGreenBufferSize(8);
-    glfmt.setBlueBufferSize(8);
-    glfmt.setDoubleBuffer(true);
-    glfmt.setDirectRendering(true);
-    QGLFormat::setDefaultFormat(glfmt);
-    
-    
-    /*
-     * Create the session manager.
-     */
-    SessionManager::createSessionManager();
-    caretLoggerIsValid = true;
-
-    /*
-     * Parameters for the program.
-     */
-    ProgramParameters* parameters = new ProgramParameters(argc, argv);
-
-    /*
-     * Log the command parameters.
-     */
-    CaretLogFine("Running: "
-                 + AString(argv[0])
-                 + " "
-                 + parameters->getAllParametersInString());
-    
-    
-    /*
-     * Create the GUI Manager.
-     */
-    GuiManager::createGuiManager();
-    
-    /*
-     * Create and display a main window.
-     * If not done as pointer, the event object is listed as an
-     * object that was not deleted by CaretObject::printListOfObjectsNotDeleted
-     * since it does not go out of scope.
-     */
-    /*EventBrowserWindowNew* newBrowserWindow = new EventBrowserWindowNew(NULL, NULL);
-    EventManager::get()->sendEvent(newBrowserWindow->getPointer());
-    delete newBrowserWindow;//*/
-    
-    //TSC: do this with explicit scoping instead of new, so that it does go out of scope.
-    //Better idea would be to have EVERYTHING except CaretObject::printListOfObjectsNotDeleted(true); in a single explicit scope
+    int result;
     {
-        EventBrowserWindowNew newBrowserWindow(NULL, NULL);
-        EventManager::get()->sendEvent(newBrowserWindow.getPointer());
+        /*
+        * Handle uncaught exceptions
+        */
+        SystemUtilities::setHandlersForUnexpected();
+        
+        qInstallMsgHandler(messageHandlerForQt);
+        QApplication app(argc, argv);
+        QApplication::setApplicationName("Connectome Workbench");
+        QApplication::setApplicationVersion("0");
+        QApplication::setOrganizationDomain("brainvis.wustl.edu");
+        QApplication::setOrganizationName("Van Essen Lab");
+        
+        /*
+        * Make sure OpenGL is available.
+        */
+        if (!QGLFormat::hasOpenGL()) { 
+            qWarning( "This system has no OpenGL support. Exiting." );
+            return -1;
+        }
+            
+        /*
+        * Setup OpenGL
+        */
+        QGLFormat glfmt;
+        glfmt.setRedBufferSize(8);
+        glfmt.setGreenBufferSize(8);
+        glfmt.setBlueBufferSize(8);
+        glfmt.setDoubleBuffer(true);
+        glfmt.setDirectRendering(true);
+        QGLFormat::setDefaultFormat(glfmt);
+        
+        
+        /*
+        * Create the session manager.
+        */
+        SessionManager::createSessionManager();
+        caretLoggerIsValid = true;
+
+        /*
+        * Parameters for the program.
+        */
+        ProgramParameters* parameters = new ProgramParameters(argc, argv);
+
+        /*
+        * Log the command parameters.
+        */
+        CaretLogFine("Running: "
+                    + AString(argv[0])
+                    + " "
+                    + parameters->getAllParametersInString());
+        
+        //begin parsing command line
+        ProgramState myState;
+        FileInformation progInfo(argv[0]);
+        AString progName = progInfo.getFileName();
+        parseCommandLine(progName, parameters, myState);
+        
+        /*
+        * Splash Screen
+        */
+        QPixmap splashPixmap;
+        QSplashScreen splashScreen;
+        if (WuQtUtilities::loadPixmap(":/splash_hcp.png", splashPixmap)) {
+            splashScreen.setPixmap(splashPixmap);
+            splashScreen.showMessage("Starting Workbench...");
+            splashScreen.show();
+            app.processEvents();
+            Sleeper::sleep(2);
+        }
+        
+        //begin using parsed command line
+        bool haveSpec = false;
+        bool haveFiles = false;
+        for (int i = 0; i < (int)myState.fileList.size(); ++i)
+        {
+            if (myState.fileList[i].endsWith(".spec"))
+            {
+                haveSpec = true;
+            } else {
+                haveFiles = true;
+            }
+        }
+        //error to have both
+        if (haveFiles && haveSpec)
+        {
+            cerr << "error, cannot specify both spec files and data files on the command line" << endl;
+            printHelp(progName);
+            return -1;
+        }
+        if (haveFiles)
+        {
+        }
+        if (haveSpec)
+        {
+            if (myState.fileList.size() > 1)
+            {
+                cerr << "error, cannot specify more than one spec file" << endl;
+                printHelp(progName);
+                return -1;
+            }
+            switch (myState.specLoadType)
+            {
+                case 0://load none
+                    break;
+                case 1://use spec dialog
+                    break;
+                case 2://load all
+                    break;
+                default:
+                    CaretAssert(false);
+            }
+        }
+        
+        /*
+        * Create the GUI Manager.
+        */
+        GuiManager::createGuiManager();
+        
+        /*
+        * Create and display a main window.
+        * If not done as pointer, the event object is listed as an
+        * object that was not deleted by CaretObject::printListOfObjectsNotDeleted
+        * since it does not go out of scope.
+        */
+        /*EventBrowserWindowNew* newBrowserWindow = new EventBrowserWindowNew(NULL, NULL);
+        EventManager::get()->sendEvent(newBrowserWindow->getPointer());
+        delete newBrowserWindow;//*/
+        
+        //TSC: do this with explicit scoping instead of new, so that it does go out of scope.
+        //Better idea would be to have EVERYTHING except CaretObject::printListOfObjectsNotDeleted(true); in a single explicit scope
+        {
+            EventBrowserWindowNew newBrowserWindow(NULL, NULL);
+            EventManager::get()->sendEvent(newBrowserWindow.getPointer());
+        }
+        //GuiManager::get()->newBrainBrowserWindow(NULL);
+        
+        if (QGLPixelBuffer::hasOpenGLPbuffers()) {
+            CaretLogConfig("OpenGL PBuffers are supported");
+        }
+        else {
+            CaretLogConfig("OpenGL PBuffers are NOT supported");
+        }
+        
+        /*
+        * Start the app which will launch the main window.
+        */
+        splashScreen.close();
+        result = app.exec();
+        
+        /*
+        * Hiding the window removes it from the event loop on Windows, which is necessary to
+        * prevent paint events from causing assertion errors when the Window is destroyed
+        * Although this is a Window's only bug, it's probably good practice to do on all platforms
+        */
+        //theMainWindow->hide();
+        
+        /*
+        * Delete the GUI Manager.
+        */
+        GuiManager::deleteGuiManager();
+        
+        /*
+        * Delete the session manager.
+        */
+        SessionManager::deleteSessionManager();
+        
+        CaretHttpManager::deleteHttpManager();
+        
+        delete parameters;
     }
-    //GuiManager::get()->newBrainBrowserWindow(NULL);
-    
-    if (QGLPixelBuffer::hasOpenGLPbuffers()) {
-        CaretLogConfig("OpenGL PBuffers are supported");
-    }
-    else {
-        CaretLogConfig("OpenGL PBuffers are NOT supported");
-    }
-    
-    /*
-     * Start the app which will launch the main window.
-     */
-    splashScreen.close();
-    int result = app.exec();
-    
-    /*
-     * Hiding the window removes it from the event loop on Windows, which is necessary to
-     * prevent paint events from causing assertion errors when the Window is destroyed
-     * Although this is a Window's only bug, it's probably good practice to do on all platforms
-     */
-    //theMainWindow->hide();
-    
-    /*
-     * Delete the GUI Manager.
-     */
-    GuiManager::deleteGuiManager();
-    
-    /*
-     * Delete the session manager.
-     */
-    SessionManager::deleteSessionManager();
-    
-    CaretHttpManager::deleteHttpManager();
-    
-    delete parameters;
-    
     /*
      * See if any objects were not deleted.
      */
     CaretObject::printListOfObjectsNotDeleted(true);
     
     return result;
+}
+
+void printHelp(const AString& progName)
+{
+    cout
+    << "Usage: " << progName << " [options] [files]" << endl
+    << endl
+    << "    [files], if present, can be a single spec file, or multiple data files" << endl
+    << endl
+    << "Options:" << endl
+    << "    -help" << endl
+    << "        display this usage text" << endl
+    << endl
+    << "    -style <style-name>" << endl
+    << "        change the window style to the specified style" << endl
+    << "        the following styles should always be valid:" << endl
+    << "        motif" << endl
+    << "        windows" << endl
+    << "        platinum" << endl
+    << endl
+    << "        other styles may be available" << endl
+    << endl
+    << "    -spec-load-all" << endl
+    << "        load all files in the given spec file, don't show spec file dialog" << endl
+    << endl
+    << "    -spec-load-none" << endl
+    << "        don't load any files from the spec file, or show spec file dialog" << endl
+    << endl;
+}
+
+void parseCommandLine(const AString& progName, ProgramParameters* myParams, ProgramState& myState)
+{
+    while (myParams->hasNext())
+    {
+        AString thisParam = myParams->nextString("option");
+        if (thisParam[0] == '-')
+        {
+            if (thisParam == "-style")
+            {
+                myParams->nextString("style");//discard, QApplication handles this
+            } else if (thisParam == "-help") {
+                printHelp(progName);
+                exit(0);
+            } else if (thisParam == "-spec-load-all") {
+                myState.specLoadType = 2;
+            } else if (thisParam == "-spec-load-none") {
+                myState.specLoadType = 0;
+            } else {
+                cerr << "unrecognized option \"" << thisParam << "\"" << endl;
+                printHelp(progName);
+                exit(-1);
+            }
+        } else {
+            myState.fileList.push_back(thisParam);
+        }
+    }
+}
+
+ProgramState::ProgramState()
+{
+    specLoadType = 1;//0: none, 1: use spec window, 2: all
 }
