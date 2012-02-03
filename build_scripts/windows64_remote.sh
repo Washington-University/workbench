0ef13fb67bb5a3de00ed7f0e30aa73669678c3d5
Tim Coalson <tsc5yc@mst.edu>
2012-02-03 14:36:06 -0600

    redirect windows ssh stderr to file also

--- build_scripts/windows64_remote.sh
+++ build_scripts/windows64_remote.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-ssh -v caret@128.252.37.24 'c:\dev7\windows64\caret7_source\build_scripts\windows64.bat' > remote_launch_windows64.txt
+ssh -v caret@128.252.37.24 'c:\dev7\windows64\caret7_source\build_scripts\windows64.bat' > remote_launch_windows64.txt 2>&1
 cat remote_launch_windows64.txt | mail -s 'Caret7 Windows 64 Build Result' john@brainvis.wustl.edu jschindl@brainvis.wustl.edu tsc5yc@mst.edu
