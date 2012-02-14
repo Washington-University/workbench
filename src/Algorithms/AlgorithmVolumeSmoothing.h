e49f6345838e396ee1f6ba460893c6c5dc3afc1b
Tim Coalson <tsc5yc@mst.edu>
2012-02-13 19:48:15 -0600

    removed instructions comment from new algorithm

--- src/Algorithms/AlgorithmVolumeSmoothing.h
+++ src/Algorithms/AlgorithmVolumeSmoothing.h
@@ -25,44 +25,6 @@
  *
  */
 
-/*
-file->save as... and enter what you will name the class, plus .h
-
-find and replace these strings, without matching "whole word only" (plain text mode):
-
-AlgorithmVolumeSmoothing     : algorithm name, in CamelCase, with initial capital, same as what you saved the header file to
-ALGORITHM_VOLUME_SMOOTHING    : uppercase of algorithm name, with underscore between words, used in #ifdef guards
--volume-smoothing   : switch for the command line to use, often hyphenated version of algorithm name, lowercase, minus "algorithm"
-SMOOTH A VOLUME FILE : short description of the command, uppercase, three to five words, often just command switch with more verbosity
-
-if the algorithm takes no parameters (???) uncomment the line below for takesParameters(), otherwise delete it
-
-next, make AlgorithmVolumeSmoothing.cxx from AlgorithmTemplate.cxx.txt via one of the following (depending on working directory):
-
-cat AlgorithmTemplate.cxx.txt | sed 's/[A]lgorithmName/AlgorithmVolumeSmoothing/g' | sed 's/-[c]ommand-switch/-volume-smoothing/g' | sed 's/[S]HORT DESCRIPTION/SMOOTH A VOLUME FILE/g' > AlgorithmVolumeSmoothing.cxx
-cat Algorithms/AlgorithmTemplate.cxx.txt | sed 's/[A]lgorithmName/AlgorithmVolumeSmoothing/g' | sed 's/-[c]ommand-switch/-volume-smoothing/g' | sed 's/[S]HORT DESCRIPTION/SMOOTH A VOLUME FILE/g' > Algorithms/AlgorithmVolumeSmoothing.cxx
-cat src/Algorithms/AlgorithmTemplate.cxx.txt | sed 's/[A]lgorithmName/AlgorithmVolumeSmoothing/g' | sed 's/-[c]ommand-switch/-volume-smoothing/g' | sed 's/[S]HORT DESCRIPTION/SMOOTH A VOLUME FILE/g' > src/Algorithms/AlgorithmVolumeSmoothing.cxx
-
-or manually copy and replace
-
-next, implement its functions - the algorithm work goes in the CONSTRUCTOR
-
-add these into Algorithms/CMakeLists.txt:
-
-AlgorithmVolumeSmoothing.h
-AlgorithmVolumeSmoothing.cxx
-
-place the following lines into Commands/CommandOperationManager.cxx:
-
-#include "AlgorithmVolumeSmoothing.h"
-    //near the top
-
-    this->commandOperations.push_back(new CommandParser(new AutoAlgorithmVolumeSmoothing()));
-        //in CommandOperationManager()
-
-finally, remove this block comment
-*/
-
 #include "AbstractAlgorithm.h"
 
 namespace caret {
