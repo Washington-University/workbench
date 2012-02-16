53ff4e8cbb24e27637a02bc2eea24a8acdd0b29f
Caret <john@brainmap.wustl.edu>
2012-02-15 18:48:08 -0600

    Updated build scripts to point to new linux build server.

--- build_scripts/linux32_remote.sh
+++ build_scripts/linux32_remote.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 BUILD=linux32
-BUILD_SERVER=128.252.37.32
+BUILD_SERVER=linuxbuild
 
 BUILD_DIR=/home/caret/${BUILD}/caret7_dev
 ssh -v caret@${BUILD_SERVER} "/bin/bash -c \"cd ${BUILD_DIR};./build32.sh $1\"" > $PWD/remote_launch_${BUILD}.txt 2>&1
--- build_scripts/linux64_remote.sh
+++ build_scripts/linux64_remote.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 BUILD=linux64
-BUILD_SERVER=128.252.37.32
+BUILD_SERVER=linuxbuild
 
 BUILD_DIR=/home/caret/${BUILD}/caret7_dev
 ssh -v caret@${BUILD_SERVER} "/bin/bash -c \"cd ${BUILD_DIR};./build64.sh $1\"" > $PWD/remote_launch_${BUILD}.txt 2>&1
