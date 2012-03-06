ae741b0ca0869837daca273f099fede1d77a399d
caret <caret@myelin1.(none)>
2012-03-06 16:00:23 -0600

    Updated build scripts to use mailx instead of mail for buildscript mail messages.

--- build_scripts/linux64_remote.sh
+++ build_scripts/linux64_remote.sh
@@ -4,5 +4,5 @@ BUILD_SERVER=linuxbuild
 
 BUILD_DIR=/home/caret/${BUILD}/caret7_dev
 ssh -v caret@${BUILD_SERVER} "/bin/bash -c \"cd ${BUILD_DIR};./build64.sh $1\"" > $PWD/remote_launch_${BUILD}.txt 2>&1
-cat $PWD/remote_launch_${BUILD}.txt | mail -s 'Caret7 Linux 64 Build Result' john@brainvis.wustl.edu jschindl@brainvis.wustl.edu tsc5yc@mst.edu
+cat $PWD/remote_launch_${BUILD}.txt | mailx -s 'Caret7 Linux 64 Build Result' john@brainvis.wustl.edu jschindl@brainvis.wustl.edu tsc5yc@mst.edu
 
--- build_scripts/windows32_remote.sh
+++ build_scripts/windows32_remote.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 ssh -v caret@windowsbuild 'c:\dev7\windows32\caret7_source\build_scripts\windows32.bat' > $PWD/remote_launch_windows32.txt 2>&1
-cat $PWD/remote_launch_windows32.txt | mail -s 'Caret7 Windows 32 Build Result' john@brainvis.wustl.edu jschindl@brainvis.wustl.edu tsc5yc@mst.edu
+cat $PWD/remote_launch_windows32.txt | mailx -s 'Caret7 Windows 32 Build Result' john@brainvis.wustl.edu jschindl@brainvis.wustl.edu tsc5yc@mst.edu
