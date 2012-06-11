7732cb99f47dbfa03bf2c13ce799262def8bebe5
John Harwell <john@brainmap.wustl.edu>
2012-06-11 15:10:58 -0500

    Removed print statement

--- src/GuiQt/WuQDialog.cxx
+++ src/GuiQt/WuQDialog.cxx
@@ -582,7 +582,7 @@ WuQDialog::disableAutoDefaultForAllPushButtons()
     QListIterator<QPushButton*> allPushButtonsIterator(allPushButtons);
     while (allPushButtonsIterator.hasNext()) {
         QPushButton* pushButton = allPushButtonsIterator.next();
-        std::cout << "Disabling auto default for: " << qPrintable(pushButton->text()) << std::endl;
+        //std::cout << "Disabling auto default for: " << qPrintable(pushButton->text()) << std::endl;
         pushButton->setAutoDefault(false);
         pushButton->setDefault(false);
     }
