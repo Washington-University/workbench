0b967ff8f9ed72b5f5efc8276643bd98e2c5a1ee
Tim Coalson <tsc5yc@mst.edu>
2012-09-25 19:37:28 -0500

    new operation to output the closest node to each coordinate in a text file

--- src/Commands/CommandOperationManager.cxx
+++ src/Commands/CommandOperationManager.cxx
@@ -63,6 +63,7 @@
 #include "OperationMetricMask.h"
 #include "OperationMetricPalette.h"
 #include "OperationSetStructure.h"
+#include "OperationSurfaceClosestNode.h"
 #include "OperationSurfaceGeodesicROIs.h"
 #include "OperationVolumeLabelImport.h"
 #include "OperationVolumePalette.h"
@@ -155,6 +156,7 @@ CommandOperationManager::CommandOperationManager()
     this->commandOperations.push_back(new CommandParser(new AutoOperationMetricMask()));
     this->commandOperations.push_back(new CommandParser(new AutoOperationMetricPalette()));
     this->commandOperations.push_back(new CommandParser(new AutoOperationSetStructure()));
+    this->commandOperations.push_back(new CommandParser(new AutoOperationSurfaceClosestNode()));
     this->commandOperations.push_back(new CommandParser(new AutoOperationSurfaceGeodesicROIs()));
     this->commandOperations.push_back(new CommandParser(new AutoOperationVolumeLabelImport()));
     this->commandOperations.push_back(new CommandParser(new AutoOperationVolumePalette()));
