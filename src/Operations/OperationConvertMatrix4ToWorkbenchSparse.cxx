9e18e004b4e8f15c173cef91d83ee5c86b5ec297
Tim Coalson <tsc5yc@mst.edu>
2012-10-05 17:54:52 -0500

    use fiber orientations file as input in -convert-matrix4-to-workbench-sparse so that the CIFTI mapping matches

--- src/Operations/OperationConvertMatrix4ToWorkbenchSparse.cxx
+++ src/Operations/OperationConvertMatrix4ToWorkbenchSparse.cxx
@@ -27,6 +27,7 @@
 
 #include "CaretHeap.h"
 #include "CaretSparseFile.h"
+#include "CiftiFile.h"
 #include "OxfordSparseThreeFile.h"
 #include "MetricFile.h"
 #include "VolumeFile.h"
@@ -55,10 +56,10 @@ OperationParameters* OperationConvertMatrix4ToWorkbenchSparse::getParameters()
     ret->addStringParameter(1, "matrix4_1", "the first matrix4 file");
     ret->addStringParameter(2, "matrix4_2", "the second matrix4 file");
     ret->addStringParameter(3, "matrix4_3", "the third matrix4 file");
-    ret->addVolumeParameter(4, "label-vol", "a label volume containing the desired structures for the white matter");
+    ret->addCiftiParameter(4, "orientation-file", "the .fiberTEMP.nii file this trajectory file applies to");
     ret->addStringParameter(5, "voxel-list", "list of voxel index triplets as used in the trajectory matrix");
     ret->addMetricParameter(6, "seed-roi", "metric roi file of all nodes used in the seed space");
-    ret->addStringParameter(7, "wb-sparse-out", "the output workbench sparse file");
+    ret->addStringParameter(7, "wb-sparse-out", "output - the output workbench sparse file");
     ret->setHelpText(
         AString("This is where you set the help text.  DO NOT add the info about what the command line format is, ") +
         "and do not give the command switch, short description, or the short descriptions of parameters.  Do not indent, " +
@@ -73,16 +74,12 @@ void OperationConvertMatrix4ToWorkbenchSparse::useParameters(OperationParameters
     AString inFile1 = myParams->getString(1);
     AString inFile2 = myParams->getString(2);
     AString inFile3 = myParams->getString(3);
-    VolumeFile* labelVol = myParams->getVolume(4);
     AString voxelFileName = myParams->getString(5);
     MetricFile* myROI = myParams->getMetric(6);
+    CiftiFile* orientationFile = myParams->getCifti(4);
     AString outFileName = myParams->getString(7);
     OxfordSparseThreeFile inFile(inFile1, inFile2, inFile3);
     const int64_t* sparseDims = inFile.getDimensions();
-    if (labelVol->getType() != SubvolumeAttributes::LABEL)
-    {
-        throw OperationException("input volume is not a label volume");
-    }
     int numNodes = myROI->getNumberOfNodes();
     int nodeCount = 0;
     const float* roiData = myROI->getValuePointerForColumn(0);
@@ -99,8 +96,10 @@ void OperationConvertMatrix4ToWorkbenchSparse::useParameters(OperationParameters
     {
         throw OperationException("failed to open voxel list file for reading");
     }
-    vector<int64_t> volDims;
-    labelVol->getDimensions(volDims);
+    int64_t volDims[3];
+    vector<vector<float> > sform;
+    CiftiXML myXML = orientationFile->getCiftiXML();
+    myXML.getVolumeDimsAndSForm(volDims, sform);
     vector<int64_t> voxelIndices;
     int64_t ind1, ind2, ind3;
     while (voxelFile >> ind1 >> ind2 >> ind3)
@@ -112,79 +111,28 @@ void OperationConvertMatrix4ToWorkbenchSparse::useParameters(OperationParameters
         voxelIndices.push_back(ind3);
     }
     if ((int64_t)voxelIndices.size() != sparseDims[0] * 3) throw OperationException("voxel list file contains the wrong number of voxels");
-    CiftiXML myXML;
+    myXML.applyColumnMapToRows();
     myXML.resetColumnsToBrainModels();
-    myXML.resetRowsToBrainModels();
     myXML.addSurfaceModelToColumns(numNodes, myROI->getStructure(), roiData);
-    myXML.setVolumeDimsAndSForm(volDims.data(), labelVol->getVolumeSpace());
     CaretAssert(myXML.getNumberOfRows() == sparseDims[1]);
-    map<int, StructureEnum::Enum> labelToStruct;
-    map<StructureEnum::Enum, pair<vector<voxelIndexType>, vector<int64_t> > > structToVoxList;
-    GiftiLabelTable* myLabels = labelVol->getMapLabelTable(0);
-    vector<int32_t> myKeys;
-    myLabels->getKeys(myKeys);
-    bool ok = false;
-    for (int i = 0; i < (int)myKeys.size(); ++i)
+    vector<vector<vector<int64_t> > > voxToOutSpace(volDims[0], vector<vector<int64_t> >(volDims[1], vector<int64_t>(volDims[2], -1)));//basically, a volume file of integers initialized to -1
+    vector<CiftiVolumeMap> myMap;
+    myXML.getVolumeMapForRows(myMap);
+    for (int64_t i = 0; i < (int64_t)myMap.size(); ++i)
     {
-        StructureEnum::Enum myStruct = StructureEnum::fromName(myLabels->getLabelName(myKeys[i]), &ok);
-        if (ok)
-        {
-            labelToStruct[myKeys[i]] = myStruct;
-        }
-    }
-    for (int i = 0; i < (int)voxelIndices.size(); i += 3)
-    {
-        float myVal = labelVol->getValue(voxelIndices.data() + i);
-        int labelKey = (int)floor(myVal + 0.5f);
-        map<int, StructureEnum::Enum>::iterator myiter = labelToStruct.find(labelKey);
-        if (myiter != labelToStruct.end())
-        {
-            vector<voxelIndexType>& myRef = structToVoxList[myiter->second].first;
-            vector<int64_t>& myRef2 = structToVoxList[myiter->second].second;
-            myRef.push_back(voxelIndices[i]);
-            myRef.push_back(voxelIndices[i + 1]);
-            myRef.push_back(voxelIndices[i + 2]);
-            myRef2.push_back(i / 3);
-        }
-    }
-    for (map<StructureEnum::Enum, pair<vector<voxelIndexType>, vector<int64_t> > >::iterator myiter = structToVoxList.begin(); myiter != structToVoxList.end(); ++myiter)
-    {
-        myXML.addVolumeModelToRows(myiter->second.first, myiter->first);
-    }
-    int64_t outDims[2] = { myXML.getNumberOfColumns(), myXML.getNumberOfRows() };
-    vector<int64_t> rowReorderReverse(outDims[0]);
-    int64_t curCount = 0;
-    for (map<StructureEnum::Enum, pair<vector<voxelIndexType>, vector<int64_t> > >::iterator myiter = structToVoxList.begin(); myiter != structToVoxList.end(); ++myiter)
-    {
-        vector<int64_t>& myRef = myiter->second.second;
-        for (int i = 0; i < (int)myRef.size(); ++i)
-        {
-            rowReorderReverse[curCount] = myRef[i];
-            ++curCount;
-        }
+        voxToOutSpace[myMap[i].m_ijk[0]][myMap[i].m_ijk[1]][myMap[i].m_ijk[2]] = myMap[i].m_ciftiIndex;//now, given voxel indices, we can get the correct output index
     }
     vector<int64_t> rowReorder(sparseDims[0], -1);
-    for (int64_t i = 0; i < outDims[0]; ++i)
+    for (int64_t i = 0; i < (int64_t)voxelIndices.size(); i += 3)
     {
-        rowReorder[rowReorderReverse[i]] = i;
-    }
-    CaretAssert(curCount == outDims[0]);
-    CaretSparseFileWriter mywriter(outFileName, outDims, myXML);//NOTE: CaretSparseFile has a different encoding of fibers, ALWAYS use getFibersRow, etc
-    /*vector<FiberFractions> inRow(sparseDims[0]), outRow(outDims[0]);//this method scans the full matrix, oblivious to sparseness, so is slow
-    for (int64_t i = 0; i < sparseDims[1]; ++i)
-    {
-        inFile.getFibersRow(i, inRow.data());
-        for (int64_t j = 0; j < outDims[0]; ++j)
+        int64_t tempInd = voxToOutSpace[voxelIndices[i]][voxelIndices[i + 1]][voxelIndices[i + 2]];
+        if (tempInd != -1)
         {
-            if (inRow[rowReorderReverse[j]].totalCount > 0)
-            {
-                outRow[j] = inRow[rowReorderReverse[j]];
-            } else {
-                outRow[j].zero();
-            }
+            rowReorder[i / 3] = tempInd;
         }
-        mywriter.writeFibersRow(i, outRow.data());
-    }//*/
+    }
+    int64_t outDims[2] = { myXML.getNumberOfColumns(), myXML.getNumberOfRows() };
+    CaretSparseFileWriter mywriter(outFileName, outDims, myXML);//NOTE: CaretSparseFile has a different encoding of fibers, ALWAYS use getFibersRow, etc
     vector<int64_t> indicesIn, indicesOut;//this method knows about sparseness, does sorting of indexes in order to avoid scanning full rows
     vector<FiberFractions> fibersIn, fibersOut;//can be slower if matrix isn't very sparse, but that is a problem for other reasons anyway
     CaretMinHeap<FiberFractions, int64_t> myHeap;//use our heap to do heapsort, rather than coding a struct for stl sort
