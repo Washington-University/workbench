edd56833633d15f5dfa5764b1c2517d4bfe4e77f
Tim Coalson <tsc5yc@mst.edu>
2012-12-28 17:59:51 -0600

    clean up comment in new -command-, not new volume

--- src/Operations/OperationSurfaceVertexAreas.h
+++ src/Operations/OperationSurfaceVertexAreas.h
@@ -25,44 +25,6 @@
  *
  */
 
-/*
-file->save as... and enter what you will name the class, plus .h
-
-if the operation takes no mandatory parameters, uncomment the line below for takesParameters(), otherwise delete it
-
-find and replace these strings, without matching "whole word only" (plain text mode):
-
-OperationSurfaceVertexAreas     : operation name, in CamelCase, with initial capital, same as what you saved the header file to
-OPERATION_SURFACE_VERTEX_AREAS    : uppercase of operation name, with underscore between words, used in #ifdef guards
--surface-vertex-areas   : switch for the command line to use, often hyphenated version of operation name, lowercase, minus "operation"
-MEASURE SURFACE AREA EACH VERTEX IS RESPONSIBLE FOR : short description of the command, uppercase, three to five words, often just command switch with more verbosity
-
-next, make OperationSurfaceVertexAreas.cxx from AlgorithmTemplate.cxx.txt via one of the following (depending on working directory):
-
-cat OperationTemplate.cxx.txt | sed 's/[O]perationName/OperationSurfaceVertexAreas/g' | sed 's/-[c]ommand-switch/-surface-vertex-areas/g' | sed 's/[S]HORT DESCRIPTION/MEASURE SURFACE AREA EACH VERTEX IS RESPONSIBLE FOR/g' > OperationSurfaceVertexAreas.cxx
-cat Operations/OperationTemplate.cxx.txt | sed 's/[O]perationName/OperationSurfaceVertexAreas/g' | sed 's/-[c]ommand-switch/-surface-vertex-areas/g' | sed 's/[S]HORT DESCRIPTION/MEASURE SURFACE AREA EACH VERTEX IS RESPONSIBLE FOR/g' > Operations/OperationSurfaceVertexAreas.cxx
-cat src/Operations/OperationTemplate.cxx.txt | sed 's/[O]perationName/OperationSurfaceVertexAreas/g' | sed 's/-[c]ommand-switch/-surface-vertex-areas/g' | sed 's/[S]HORT DESCRIPTION/MEASURE SURFACE AREA EACH VERTEX IS RESPONSIBLE FOR/g' > src/Operations/OperationSurfaceVertexAreas.cxx
-
-or manually copy and replace
-
-next, implement its functions
-
-add these to Operations/CMakeLists.txt:
-
-OperationSurfaceVertexAreas.h
-OperationSurfaceVertexAreas.cxx
-
-place the following lines into Commands/CommandOperationManager.cxx:
-
-#include "OperationSurfaceVertexAreas.h"
-    //near the top
-
-    this->commandOperations.push_back(new CommandParser(new AutoOperationSurfaceVertexAreas()));
-        //in CommandOperationManager()
-
-finally, remove this block comment
-*/
-
 #include "AbstractOperation.h"
 
 namespace caret {
