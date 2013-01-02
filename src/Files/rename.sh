05b2166f8350e5d563f1fe98c5c77f045fa06ee5
John Harwell <john@brainmap.wustl.edu>
2013-01-02 09:31:17 -0600

    Removed script that mistakenly added to repository

--- src/Files/rename.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mv CiftiDenseParcelFile.h  CiftiConnectivityMatrixDenseParcelFile.h
-mv CiftiDenseParcelFile.cxx  CiftiConnectivityMatrixDenseParcelFile.cxx
-mv CiftiParcelDenseFile.h  CiftiConnectivityMatrixParcelDenseFile.h
-mv CiftiParcelDenseFile.cxx  CiftiConnectivityMatrixParcelDenseFile.cxx
-mv CiftiParcelFile.h  CiftiConnectivityMatrixParcelFile.h
-mv CiftiParcelFile.cxx  CiftiConnectivityMatrixParcelFile.cxx
