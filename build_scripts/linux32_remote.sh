e295af9df2c81366f71f573a0fa4017f08a498aa
Jon Schindler <jschindl@brainvis.wustl.edu>
2013-01-22 10:17:39 -0600

    The linux 32 and 64 build scripts in myelin1:/mainpool/storage/build/caret7_development/caret7_source/build_scripts appear to be used to run the autobuilds.  Needs to pull source to get updates to Mac scripts since mac building is being moved from DS4600 to /Users/caret

--- build_scripts/linux32_remote.sh
+++ build_scripts/linux32_remote.sh
@@ -3,5 +3,5 @@ BUILD=linux32
 BUILD_SERVER=linuxbuild
 
 BUILD_DIR=/home/caret/${BUILD}/caret7_dev
-ssh -v caret@${BUILD_SERVER} "/bin/bash -c \"cd ${BUILD_DIR};./build32.sh $1\"" > $PWD/remote_launch_${BUILD}.txt 2>&1
+ssh -v caret@${BUILD_SERVER} "/bin/bash -c \"cd ${BUILD_DIR};./build32.sh caret\"" > $PWD/remote_launch_${BUILD}.txt 2>&1
 cat $PWD/remote_launch_${BUILD}.txt | mailx -s 'Caret7 Linux 32 Build Result' john@brainvis.wustl.edu jschindl@brainvis.wustl.edu tsc5yc@mst.edu
--- build_scripts/linux64_remote.sh
+++ build_scripts/linux64_remote.sh
@@ -3,6 +3,6 @@ BUILD=linux64
 BUILD_SERVER=linuxbuild
 
 BUILD_DIR=/home/caret/${BUILD}/caret7_dev
-ssh -v caret@${BUILD_SERVER} "/bin/bash -c \"cd ${BUILD_DIR};./build64.sh $1\"" > $PWD/remote_launch_${BUILD}.txt 2>&1
+ssh -v caret@${BUILD_SERVER} "/bin/bash -c \"cd ${BUILD_DIR};./build64.sh caret\"" > $PWD/remote_launch_${BUILD}.txt 2>&1
 cat $PWD/remote_launch_${BUILD}.txt | mailx -s 'Caret7 Linux 64 Build Result' john@brainvis.wustl.edu jschindl@brainvis.wustl.edu tsc5yc@mst.edu
 
