4a0d031224d43240da0737788db45c8473d4e33a
Jon Schindler <jschindl@brainvis.wustl.edu>
2013-02-27 15:30:29 -0600

    Added new redhat linux build scripts for supercomputer build.

---
+++ build_scripts/linux64_rh.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+#
+# This file is executed by myelin1.
+#    
+#
+# (1) Update source code from repository
+# (2) Build Caret programs
+# (3) Copy executables into the distribution
+#
+
+#
+# File for capturing standard error
+#
+BUILD_ROOT_DIR=/home/caret/caret7_dev
+ERROR_FILE=${BUILD_ROOT_DIR}/result_build_linux64_rh.txt
+rm -f ${ERROR_FILE}
+
+#
+# If something fails, keep going
+#
+set noon
+
+#
+# Run the build script
+#
+echo "Starting linux64 Redhat aux script" >> ${ERROR_FILE} 2>&1
+${BUILD_ROOT_DIR}/caret7_source/build_scripts/linux64_rh_aux.sh >> ${ERROR_FILE} 2>&1
+echo "Finished linux64 Redhat script" >> ${ERROR_FILE} 2>&1
+
+#
+# Send output as email
+#
+MAIL_ERROR_FILE=${BUILD_ROOT_DIR}/mail_result_build_linux64_rh.txt
+rm -f ${MAIL_ERROR_FILE}
+#cat ${ERROR_FILE} | mail -v -s 'Caret7 Linux 64 Intel Build Result' john@brainvis.wustl.edu jschindl@brainvis.wustl.edu tsc5yc@mst.edu >> ${MAIL_ERROR_FILE} 2>&1
+cat ${ERROR_FILE} | mail -v -s 'Caret7 Linux 64 Intel Build Result' jschindl@brainvis.wustl.edu >> ${MAIL_ERROR_FILE} 2>&1
+echo "Sent mail"
+
---
+++ build_scripts/linux64_rh_aux.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+#
+# This file is executed by myelin1
+#    
+#
+# (1) Update source code from repository
+# (2) Build Caret programs
+# (3) Copy executables into the distribution
+#
+# Based off Tim Coalson's "buildbot" script
+#
+
+#
+# Go to correct directory
+#
+BUILD_ROOT_DIR=/home/caret/caret7_dev
+GIT_ROOT_DIR=${BUILD_ROOT_DIR}/caret7_source
+SRC_DIR=${GIT_ROOT_DIR}/src
+BUILD_DIR=${BUILD_ROOT_DIR}/build
+cd ${BUILD_ROOT_DIR}
+echo "BUILD_DIR: ${BUILD_DIR}"
+
+#
+# Setup Environment
+#
+
+#
+# Use Qt SDK
+#
+
+QTDIR=/home/caret/caret5_and_7_libraries/install/qt-4.8.4-shared
+export QTDIR
+echo "QTDIR: ${QTDIR}"
+
+PATH=${QTDIR}/bin:${PATH}
+export PATH
+QMAKE_PROG=`which qmake`
+echo "QMAKE: ${QMAKE_PROG}"
+
+#
+# Go into git checkout directory
+#
+cd ${GIT_ROOT_DIR}
+
+
+#
+# Update source from repository
+#
+echo "UPDATING SOURCE FROM GIT REPOSITORY"
+git reset --hard HEAD
+git pull -u
+
+#
+# Configure and build as release.
+# Build twice so that if the build fails, the second build will
+# contain just the output that shows the errors.
+# Catch output and echo to screen.
+#
+echo "BUILDING SOURCE"
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+cmake -DCMAKE_BUILD_TYPE=Release ${SRC_DIR}
+make -j2 
+make -j2
+
+#
+# Copy to distribution directory
+#
+echo "COPYING PROGRAMS"
+DIST_DIR=/mainpool/storage/distribution/caret7_distribution/workbench/exe_rh_linux64
+scp -rv Desktop/workbench caret@myelin1:${DIST_DIR}
+scp -rv CommandLine/wb_command caret@myelin1:${DIST_DIR}
+
+ssh caret@myelin1 touch ${DIST_DIR}/workbench
+
+echo "SCRIPT COMPLETED SUCCESSFULLY"
+
