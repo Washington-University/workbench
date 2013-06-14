2c86d2dd83f8b29828c2e270394d73aa62c861e7
Tim Coalson <tsc5yc@mst.edu>
2013-06-14 17:43:35 -0500

    add outlier exclusion option to -cifti-average, make it always ignore NaN/inf

--- src/Algorithms/AlgorithmCiftiAverage.cxx
+++ src/Algorithms/AlgorithmCiftiAverage.cxx
@@ -25,12 +25,12 @@
 #include "AlgorithmCiftiAverage.h"
 #include "AlgorithmException.h"
 #include "CaretAssert.h"
+#include "CaretLogger.h"
 #include "CaretPointer.h"
 #include "CiftiFile.h"
-#include "FileInformation.h"
+#include "MathFunctions.h"
 
-#include <fstream>
-#include <string>
+#include <cmath>
 
 using namespace caret;
 using namespace std;
@@ -51,14 +51,21 @@ OperationParameters* AlgorithmCiftiAverage::getParameters()
     
     ret->addCiftiOutputParameter(1, "cifti-out", "output cifti file");
     
-    ParameterComponent* ciftiOpt = ret->createRepeatableParameter(2, "-cifti", "specify an input file");
+    OptionalParameter* excludeOpt = ret->createOptionalParameter(2, "-exclude-outliers", "exclude outliers by standard deviation of each element across files");
+    excludeOpt->addDoubleParameter(1, "sigma-below", "number of standard deviations below the mean to include");
+    excludeOpt->addDoubleParameter(2, "sigma-above", "number of standard deviations above the mean to include");
+
+    ParameterComponent* ciftiOpt = ret->createRepeatableParameter(3, "-cifti", "specify an input file");
     ciftiOpt->addCiftiParameter(1, "cifti-in", "the input cifti file");
     
-    OptionalParameter* weightOpt = ciftiOpt->createOptionalParameter(2, "-weight", "give a weight for this file");
+    OptionalParameter* weightOpt = ciftiOpt->createOptionalParameter(1, "-weight", "give a weight for this file");
     weightOpt->addDoubleParameter(1, "weight", "the weight to use");
     
     ret->setHelpText(
-        AString("Averages cifti files together.  Files without -weight specified are given a weight of 1.")
+        AString("Averages cifti files together.  ") +
+        "Files without -weight specified are given a weight of 1.  " +
+        "If -exclude-outliers is specified, at each element, the data across all files is taken as a set, its unweighted mean and sample standard deviation are found, " +
+        "and values outside the specified number of standard deviations are excluded from the (potentially weighted) average at that element."
     );
     return ret;
 }
@@ -68,11 +75,11 @@ void AlgorithmCiftiAverage::useParameters(OperationParameters* myParams, Progres
     CiftiFile* ciftiOut = myParams->getOutputCifti(1);
     vector<const CiftiInterface*> ciftiList;//this is just so that it can pass them to the algorithm
     vector<float> weights;
-    const vector<ParameterComponent*>& myInstances = *(myParams->getRepeatableParameterInstances(2));
+    const vector<ParameterComponent*>& myInstances = *(myParams->getRepeatableParameterInstances(3));
     for (int i = 0; i < (int)myInstances.size(); ++i)
     {
         ciftiList.push_back(myInstances[i]->getCifti(1));
-        OptionalParameter* weightOpt = myInstances[i]->getOptionalParameter(2);
+        OptionalParameter* weightOpt = myInstances[i]->getOptionalParameter(1);
         if (weightOpt->m_present)
         {
             weights.push_back((float)weightOpt->getDouble(1));
@@ -80,7 +87,13 @@ void AlgorithmCiftiAverage::useParameters(OperationParameters* myParams, Progres
             weights.push_back(1.0f);
         }
     }
-    AlgorithmCiftiAverage(myProgObj, ciftiList, ciftiOut, &weights);//executes the algorithm
+    OptionalParameter* excludeOpt = myParams->getOptionalParameter(2);
+    if (excludeOpt->m_present)
+    {
+        AlgorithmCiftiAverage(myProgObj, ciftiList, excludeOpt->getDouble(1), excludeOpt->getDouble(2), ciftiOut, &weights);
+    } else {
+        AlgorithmCiftiAverage(myProgObj, ciftiList, ciftiOut, &weights);
+    }
 }
 
 AlgorithmCiftiAverage::AlgorithmCiftiAverage(ProgressObject* myProgObj, const vector<const CiftiInterface*>& ciftiList, CiftiFile* ciftiOut, const vector<float>* weightsPtr) : AbstractAlgorithm(myProgObj)
@@ -96,7 +109,8 @@ AlgorithmCiftiAverage::AlgorithmCiftiAverage(ProgressObject* myProgObj, const ve
     }
     CaretAssert(ciftiList[0] != NULL);
     CiftiXML baseXML = ciftiList[0]->getCiftiXML();
-    for (int i = 1; i < (int)ciftiList.size(); ++i)
+    int numRows = baseXML.getNumberOfRows(), rowSize = baseXML.getNumberOfColumns(), numFiles = (int)ciftiList.size();
+    for (int i = 1; i < numFiles; ++i)
     {
         CaretAssert(ciftiList[i] != NULL);
         if (baseXML != ciftiList[i]->getCiftiXML())
@@ -105,13 +119,11 @@ AlgorithmCiftiAverage::AlgorithmCiftiAverage(ProgressObject* myProgObj, const ve
         }
     }
     ciftiOut->setCiftiXML(baseXML);
-    int numRows = baseXML.getNumberOfRows(), rowSize = baseXML.getNumberOfColumns(), numFiles = (int)ciftiList.size();
     vector<double> accum(rowSize);
     vector<float> myrow(rowSize);
-    double weightaccum;
     for (int i = 0; i < numRows; ++i)
     {
-        weightaccum = 0.0;
+        vector<double> weightaccum(rowSize, 0.0);
         for (int j = 0; j < rowSize; ++j)
         {
             accum[j] = 0.0;
@@ -123,30 +135,132 @@ AlgorithmCiftiAverage::AlgorithmCiftiAverage(ProgressObject* myProgObj, const ve
             {
                 for (int k = 0; k < rowSize; ++k)
                 {
-                    accum[k] += myrow[k];
+                    if (MathFunctions::isNumeric(myrow[k]))
+                    {
+                        weightaccum[k] += 1.0;
+                        accum[k] += myrow[k];
+                    }
                 }
             } else {
                 float weight = (*weightsPtr)[j];
-                weightaccum += weight;
                 for (int k = 0; k < rowSize; ++k)
                 {
-                    accum[k] += myrow[k] * weight;
+                    if (MathFunctions::isNumeric(myrow[k]))
+                    {
+                        weightaccum[k] += weight;
+                        accum[k] += myrow[k] * weight;
+                    }
                 }
             }
         }
-        if (weightsPtr == NULL)
+        for (int k = 0; k < rowSize; ++k)
         {
-            for (int k = 0; k < rowSize; ++k)
+            if (weightaccum[k] != 0.0)
             {
-                myrow[k] = accum[k] / numFiles;
-            }
-        } else {
-            for (int k = 0; k < rowSize; ++k)
-            {
-                myrow[k] = accum[k] / weightaccum;
+                myrow[k] = accum[k] / weightaccum[k];
+            } else {
+                myrow[k] = 0.0f;
             }
         }
         ciftiOut->setRow(myrow.data(), i);
+    }
+}
+
+AlgorithmCiftiAverage::AlgorithmCiftiAverage(ProgressObject* myProgObj, const vector<const CiftiInterface*>& ciftiList,
+                                             const float& sigmaBelow, const float& sigmaAbove,
+                                             CiftiFile* ciftiOut, const std::vector<float>* weightsPtr): AbstractAlgorithm(myProgObj)
+{
+    LevelProgress myProgress(myProgObj);
+    if (ciftiList.size() < 2)
+    {
+        throw AlgorithmException("fewer than 2 files specified with outlier exclusion");
+    }
+    if (weightsPtr != NULL && ciftiList.size() != weightsPtr->size())
+    {
+        throw AlgorithmException("number of weights doesn't match number of input cifti files");
+    }
+    CaretAssert(ciftiList[0] != NULL);
+    CiftiXML baseXML = ciftiList[0]->getCiftiXML();
+    int numRows = baseXML.getNumberOfRows(), rowSize = baseXML.getNumberOfColumns(), numFiles = (int)ciftiList.size();
+    for (int i = 1; i < numFiles; ++i)
+    {
+        CaretAssert(ciftiList[i] != NULL);
+        if (baseXML != ciftiList[i]->getCiftiXML())
+        {
+            throw AlgorithmException("cifti files do not match");
+        }
+    }
+    bool haveWarned = false;
+    ciftiOut->setCiftiXML(baseXML);
+    vector<vector<float> > myrows(numFiles, vector<float>(rowSize));
+    for (int i = 0; i < numRows; ++i)
+    {
+        for (int j = 0; j < numFiles; ++j)
+        {
+            ciftiList[j]->getRow(myrows[j].data(), i);
+        }
+        vector<float> outrow(rowSize);
+        for (int k = 0; k < rowSize; ++k)
+        {
+            double accum = 0.0;
+            double weightaccum = 0.0;
+            int nonnumeric = 0;
+            for (int j = 0; j < numFiles; ++j)
+            {
+                if (MathFunctions::isNumeric(myrows[j][k]))
+                {
+                    accum += myrows[j][k];
+                } else {
+                    ++nonnumeric;
+                }
+            }
+            if (nonnumeric >= numFiles - 1)
+            {
+                if (!haveWarned)
+                {
+                    CaretLogWarning("found element where less than 2 files have numeric values");
+                    haveWarned = true;
+                }
+                outrow[k] = 0.0f;
+            } else {
+                float mean = accum / (numFiles - nonnumeric);
+                accum = 0.0;
+                for (int j = 0; j < numFiles; ++j)
+                {
+                    if (MathFunctions::isNumeric(myrows[j][k]))
+                    {
+                        float temp = myrows[j][k] - mean;
+                        accum += temp * temp;
+                    }
+                }
+                float stdev = sqrt(accum / (numFiles - 1 - nonnumeric));
+                float cutoffLow = mean - sigmaBelow * stdev;
+                float cutoffHigh = mean + sigmaAbove * stdev;
+                accum = 0.0;
+                for (int j = 0; j < numFiles; ++j)
+                {
+                    if (myrows[j][k] > cutoffLow && myrows[j][k] < cutoffHigh)//implicitly excludes NaN and inf
+                    {
+                        if (weightsPtr != NULL)
+                        {
+                            float weight = (*weightsPtr)[j];
+                            accum += myrows[j][k] * weight;
+                            weightaccum += weight;
+                        } else {
+                            accum += myrows[j][k];
+                            weightaccum += 1.0;
+                        }
+                    }
+                }
+                if (weightaccum != 0.0)
+                {
+                    outrow[k] = accum / weightaccum;
+                } else {
+                    outrow[k] = 0.0f;
+                }
+            }
+        }
+        ciftiOut->setRow(outrow.data(), i);
     }
 }
 
--- src/Algorithms/AlgorithmCiftiAverage.h
+++ src/Algorithms/AlgorithmCiftiAverage.h
@@ -40,6 +40,7 @@ namespace caret {
         static float getAlgorithmInternalWeight();
     public:
         AlgorithmCiftiAverage(ProgressObject* myProgObj, const std::vector<const CiftiInterface*>& ciftiList, CiftiFile* ciftiOut, const std::vector<float>* weightsPtr = NULL);
+        AlgorithmCiftiAverage(ProgressObject* myProgObj, const std::vector<const CiftiInterface*>& ciftiList, const float& sigmaBelow, const float& sigmaAbove, CiftiFile* ciftiOut, const std::vector<float>* weightsPtr = NULL);
         static OperationParameters* getParameters();
         static void useParameters(OperationParameters* myParams, ProgressObject* myProgObj);
         static AString getCommandSwitch();
