c49b0b7ab87f01df5a4f06c2a1c6a1a344bcd4c2
Tim Coalson <tsc5yc@mst.edu>
2013-06-17 15:11:49 -0500

    clean up new command header

--- src/Operations/OperationSurfaceGeodesicDistance.h
+++ src/Operations/OperationSurfaceGeodesicDistance.h
@@ -25,44 +25,6 @@
  *
  */
 
-/*
-file->save as... and enter what you will name the class, plus .h
-
-if the operation takes no mandatory parameters, uncomment the line below for takesParameters(), otherwise delete it
-
-find and replace these strings, without matching "whole word only" (plain text mode):
-
-OperationSurfaceGeodesicDistance     : operation name, in CamelCase, with initial capital, same as what you saved the header file to
-OPERATION_SURFACE_GEODESIC_DISTANCE    : uppercase of operation name, with underscore between words, used in #ifdef guards
--surface-geodesic-distance   : switch for the command line to use, often hyphenated version of operation name, lowercase, minus "operation"
-COMPUTE GEODESIC DISTANCE FROM ONE VERTEX TO THE ENTIRE SURFACE : short description of the command, uppercase, three to five words, often just command switch with more verbosity
-
-next, make OperationSurfaceGeodesicDistance.cxx from AlgorithmTemplate.cxx.txt via one of the following (depending on working directory):
-
-cat OperationTemplate.cxx.txt | sed 's/[O]perationName/OperationSurfaceGeodesicDistance/g' | sed 's/-[c]ommand-switch/-surface-geodesic-distance/g' | sed 's/[S]HORT DESCRIPTION/COMPUTE GEODESIC DISTANCE FROM ONE VERTEX TO THE ENTIRE SURFACE/g' > OperationSurfaceGeodesicDistance.cxx
-cat Operations/OperationTemplate.cxx.txt | sed 's/[O]perationName/OperationSurfaceGeodesicDistance/g' | sed 's/-[c]ommand-switch/-surface-geodesic-distance/g' | sed 's/[S]HORT DESCRIPTION/COMPUTE GEODESIC DISTANCE FROM ONE VERTEX TO THE ENTIRE SURFACE/g' > Operations/OperationSurfaceGeodesicDistance.cxx
-cat src/Operations/OperationTemplate.cxx.txt | sed 's/[O]perationName/OperationSurfaceGeodesicDistance/g' | sed 's/-[c]ommand-switch/-surface-geodesic-distance/g' | sed 's/[S]HORT DESCRIPTION/COMPUTE GEODESIC DISTANCE FROM ONE VERTEX TO THE ENTIRE SURFACE/g' > src/Operations/OperationSurfaceGeodesicDistance.cxx
-
-or manually copy and replace
-
-next, implement its functions
-
-add these to Operations/CMakeLists.txt:
-
-OperationSurfaceGeodesicDistance.h
-OperationSurfaceGeodesicDistance.cxx
-
-place the following lines into Commands/CommandOperationManager.cxx:
-
-#include "OperationSurfaceGeodesicDistance.h"
-    //near the top
-
-    this->commandOperations.push_back(new CommandParser(new AutoOperationSurfaceGeodesicDistance()));
-        //in CommandOperationManager()
-
-finally, remove this block comment
-*/
-
 #include "AbstractOperation.h"
 
 namespace caret {
