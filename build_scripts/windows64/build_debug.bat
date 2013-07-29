0d9ede8507fc3e1654bd7295c28bbe21489c4b51
John Harwell <john@brainmap.wustl.edu>
2013-07-29 12:25:59 -0500

    Added a couple of script file to build windows in 64 in debug

---
+++ build_scripts/windows64/build_debug.bat
@@ -0,0 +1,11 @@
+@echo off
+if [%MACHINE%]==[] call setup_env.bat
+
+set OLD_DIR=%CD%
+REM rd /q/s %DIR%\caret7_source\build_debug
+md %DIR%\caret7_source\build
+cd /d %DIR%\caret7_source\build
+cmake -G "NMake Makefiles JOM" -DCMAKE_BUILD_TYPE=Debug ../src
+jom -j8
+cd /d %OLD_DIR%
+set OLD_DIR=
