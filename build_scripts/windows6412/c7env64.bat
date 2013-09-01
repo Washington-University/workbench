66aa13b24585cb68e524fba7777d01703e623fe7
Jon Schindler <jschindl@brainvis.wustl.edu>
2013-08-31 23:00:51 -0500

    Fixed permissions on windows build helper scripts.

--- build_scripts/windows6412/c7env64.bat
+++ build_scripts/windows6412/c7env64.bat
@@ -1,4 +1,4 @@
-set PATH=C:\dev6411\install\Qt482\bin;C:\Qt\qtcreator-2.4.1\bin;%PATH%
-set QTDIR=C:\dev6411\install\Qt482
-set ZLIB_LIB_DIR=C:\dev6411\install\zlib\lib
-set ZLIB_INC_DIR=C:\dev6411\install\zlib\include
+set PATH=C:\dev6412\install\Qt486\bin;C:\Qt\qtcreator-2.4.1\bin;%PATH%
+set QTDIR=C:\dev6412\install\Qt486
+set ZLIB_LIB_DIR=C:\dev6412\install\zlib\lib
+set ZLIB_INC_DIR=C:\dev6412\install\zlib\include
