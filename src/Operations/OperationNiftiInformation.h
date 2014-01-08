cac34f6d4c687e499dbbe226960307d0784c9a35
Tim Coalson <tsc5yc@mst.edu>
2014-01-08 16:41:37 -0600

    remove instruction comments from -nifti-information

--- src/Operations/OperationNiftiInformation.h
+++ src/Operations/OperationNiftiInformation.h
@@ -25,44 +25,6 @@
  *
  */
 
-/*
-file->save as... and enter what you will name the class, plus .h
-
-if the operation takes no mandatory parameters, uncomment the line below for takesParameters(), otherwise delete it
-
-find and replace these strings, without matching "whole word only" (plain text mode):
-
-OperationNiftiInformation     : operation name, in CamelCase, with initial capital, same as what you saved the header file to
-OPERATION_NIFTI_INFORMATION    : uppercase of operation name, with underscore between words, used in #ifdef guards
--nifti-information   : switch for the command line to use, often hyphenated version of operation name, lowercase, minus "operation"
-DISPLAY INFORMATION ABOUT A NIFTI/CIFTI FILE : short description of the command, uppercase, three to five words, often just command switch with more verbosity
-
-next, make OperationNiftiInformation.cxx from AlgorithmTemplate.cxx.txt via one of the following (depending on working directory):
-
-cat OperationTemplate.cxx.txt | sed 's/[O]perationName/OperationNiftiInformation/g' | sed 's/-[c]ommand-switch/-nifti-information/g' | sed 's/[S]HORT DESCRIPTION/DISPLAY INFORMATION ABOUT A NIFTI/CIFTI FILE/g' > OperationNiftiInformation.cxx
-cat Operations/OperationTemplate.cxx.txt | sed 's/[O]perationName/OperationNiftiInformation/g' | sed 's/-[c]ommand-switch/-nifti-information/g' | sed 's/[S]HORT DESCRIPTION/DISPLAY INFORMATION ABOUT A NIFTI/CIFTI FILE/g' > Operations/OperationNiftiInformation.cxx
-cat src/Operations/OperationTemplate.cxx.txt | sed 's/[O]perationName/OperationNiftiInformation/g' | sed 's/-[c]ommand-switch/-nifti-information/g' | sed 's|[S]HORT DESCRIPTION|DISPLAY INFORMATION ABOUT A NIFTI/CIFTI FILE|g' > src/Operations/OperationNiftiInformation.cxx
-
-or manually copy and replace
-
-next, implement its functions
-
-add these to Operations/CMakeLists.txt:
-
-OperationNiftiInformation.h
-OperationNiftiInformation.cxx
-
-place the following lines into Commands/CommandOperationManager.cxx:
-
-#include "OperationNiftiInformation.h"
-    //near the top
-
-    this->commandOperations.push_back(new CommandParser(new AutoOperationNiftiInformation()));
-        //in CommandOperationManager()
-
-finally, remove this block comment
-*/
-
 #include "AbstractOperation.h"
 
 namespace caret {
@@ -74,7 +36,6 @@ namespace caret {
         static void useParameters(OperationParameters* myParams, ProgressObject* myProgObj);
         static AString getCommandSwitch();
         static AString getShortDescription();
-        //static bool takesParameters() { return false; };
     };
 
     typedef TemplateAutoOperation<OperationNiftiInformation> AutoOperationNiftiInformation;
