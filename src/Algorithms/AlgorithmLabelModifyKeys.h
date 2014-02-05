24a728775f9406cbd560a65a7cd18c52a088268d
Tim Coalson <tsc5yc@mst.edu>
2014-02-05 14:41:16 -0600

    clean up instruction comments from new algorithm header

--- src/Algorithms/AlgorithmLabelModifyKeys.h
+++ src/Algorithms/AlgorithmLabelModifyKeys.h
@@ -25,42 +25,6 @@
  *
  */
 
-/*
-file->save as... and enter what you will name the class, plus .h
-
-find and replace these strings, without matching "whole word only" (plain text mode):
-
-AlgorithmLabelModifyKeys     : algorithm name, in CamelCase, with initial capital, same as what you saved the header file to
-ALGORITHM_LABEL_MODIFY_KEYS    : uppercase of algorithm name, with underscore between words, used in #ifdef guards
--label-modify-keys   : switch for the command line to use, often hyphenated version of algorithm name, lowercase, minus "algorithm"
-CHANGE KEY VALUES IN A LABEL FILE : short description of the command, uppercase, three to five words, often just command switch with more verbosity
-
-next, make AlgorithmLabelModifyKeys.cxx from AlgorithmTemplate.cxx.txt via one of the following (depending on working directory):
-
-cat AlgorithmTemplate.cxx.txt | sed 's/[A]lgorithmName/AlgorithmLabelModifyKeys/g' | sed 's/-[c]ommand-switch/-label-modify-keys/g' | sed 's/[S]HORT DESCRIPTION/CHANGE KEY VALUES IN A LABEL FILE/g' > AlgorithmLabelModifyKeys.cxx
-cat Algorithms/AlgorithmTemplate.cxx.txt | sed 's/[A]lgorithmName/AlgorithmLabelModifyKeys/g' | sed 's/-[c]ommand-switch/-label-modify-keys/g' | sed 's/[S]HORT DESCRIPTION/CHANGE KEY VALUES IN A LABEL FILE/g' > Algorithms/AlgorithmLabelModifyKeys.cxx
-cat src/Algorithms/AlgorithmTemplate.cxx.txt | sed 's/[A]lgorithmName/AlgorithmLabelModifyKeys/g' | sed 's/-[c]ommand-switch/-label-modify-keys/g' | sed 's|[S]HORT DESCRIPTION|CHANGE KEY VALUES IN A LABEL FILE|g' > src/Algorithms/AlgorithmLabelModifyKeys.cxx
-
-or manually copy and replace
-
-next, implement its functions - the algorithm work goes in the CONSTRUCTOR
-
-add these into Algorithms/CMakeLists.txt:
-
-AlgorithmLabelModifyKeys.h
-AlgorithmLabelModifyKeys.cxx
-
-place the following lines into Commands/CommandOperationManager.cxx:
-
-#include "AlgorithmLabelModifyKeys.h"
-    //near the top
-
-    this->commandOperations.push_back(new CommandParser(new AutoAlgorithmLabelModifyKeys()));
-        //in CommandOperationManager()
-
-finally, remove this block comment
-*/
-
 #include "AbstractAlgorithm.h"
 
 #include <map>
