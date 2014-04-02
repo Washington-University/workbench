4dd4ca92b602e9cf6473b4f8e3855698503099bc
Tim Coalson <tsc5yc@mst.edu>
2014-04-01 20:33:18 -0500

    change redhat build script to not rerun cmake or mess with environment

--- build_scripts/linux64_rh_aux.sh
+++ build_scripts/linux64_rh_aux.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #
-# This file is executed by myelin1
+# This file is executed on the redhat build machine
 #    
 #
 # (1) Update source code from repository
@@ -11,32 +11,11 @@
 # Based off Tim Coalson's "buildbot" script
 #
 
-#
-# Go to correct directory
-#
 BUILD_ROOT_DIR=/home/caret/caret7_dev
 GIT_ROOT_DIR=${BUILD_ROOT_DIR}/caret7_source
-SRC_DIR=${GIT_ROOT_DIR}/src
 BUILD_DIR=${BUILD_ROOT_DIR}/build
 cd ${BUILD_ROOT_DIR}
 echo "BUILD_DIR: ${BUILD_DIR}"
-
-#
-# Setup Environment
-#
-
-#
-# Use Qt SDK
-#
-
-QTDIR=/home/caret/caret5_and_7_libraries/install/qt-4.8.4-shared
-export QTDIR
-echo "QTDIR: ${QTDIR}"
-
-PATH=${QTDIR}/bin:${PATH}
-export PATH
-QMAKE_PROG=`which qmake`
-echo "QMAKE: ${QMAKE_PROG}"
 
 #
 # Go into git checkout directory
@@ -48,20 +27,14 @@ cd ${GIT_ROOT_DIR}
 # Update source from repository
 #
 echo "UPDATING SOURCE FROM GIT REPOSITORY"
-git reset --hard HEAD
-git pull -u
+git fetch
+git reset --hard origin/master
 
 #
-# Configure and build as release.
-# Build twice so that if the build fails, the second build will
-# contain just the output that shows the errors.
-# Catch output and echo to screen.
+# Build, assume the build directory is already set up as desired
 #
 echo "BUILDING SOURCE"
-mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
-cmake -DCMAKE_BUILD_TYPE=Release ${SRC_DIR}
-make -j2 
 make -j2
 
 #
@@ -71,8 +44,6 @@ echo "COPYING PROGRAMS"
 DIST_DIR=/mainpool/storage/distribution/caret7_distribution/workbench/exe_rh_linux64
 scp -rv Desktop/workbench caret@myelin1.wustl.edu:${DIST_DIR}
 scp -rv CommandLine/wb_command caret@myelin1.wustl.edu:${DIST_DIR}
-
-ssh caret@myelin1.wustl.edu touch ${DIST_DIR}/workbench
 
 echo "SCRIPT COMPLETED SUCCESSFULLY"
 
