1ba620c5a376dfb1cbd60f0e9bfed3d901544763
Tim Coalson <tsc5yc@mst.edu>
2014-04-11 18:18:38 -0500

    remove another lingering instruction comment

--- src/Operations/OperationCiftiEstimateFWHM.h
+++ src/Operations/OperationCiftiEstimateFWHM.h
@@ -21,42 +21,6 @@
  */
 /*LICENSE_END*/
 
-/*
-file->save as... and enter what you will name the class, plus .h
-
-find and replace these strings, without matching "whole word only" (plain text mode):
-
-OperationCiftiEstimateFWHM     : operation name, in CamelCase, with initial capital, same as what you saved the header file to
-OPERATION_CIFTI_ESTIMATE_FWHM    : uppercase of operation name, with underscore between words, used in #ifdef guards
--cifti-estimate-fwhm   : switch for the command line to use, often hyphenated version of operation name, lowercase, minus "operation"
-ESTIMATE FWHM SMOOTHNESS OF A CIFTI FILE : short description of the command, uppercase, three to five words, often just command switch with more verbosity
-
-next, make OperationCiftiEstimateFWHM.cxx from OperationTemplate.cxx.txt via one of the following (depending on working directory):
-
-cat OperationTemplate.cxx.txt | sed 's/[O]perationName/OperationCiftiEstimateFWHM/g' | sed 's/-[c]ommand-switch/-cifti-estimate-fwhm/g' | sed 's/[S]HORT DESCRIPTION/ESTIMATE FWHM SMOOTHNESS OF A CIFTI FILE/g' > OperationCiftiEstimateFWHM.cxx
-cat Operations/OperationTemplate.cxx.txt | sed 's/[O]perationName/OperationCiftiEstimateFWHM/g' | sed 's/-[c]ommand-switch/-cifti-estimate-fwhm/g' | sed 's/[S]HORT DESCRIPTION/ESTIMATE FWHM SMOOTHNESS OF A CIFTI FILE/g' > Operations/OperationCiftiEstimateFWHM.cxx
-cat src/Operations/OperationTemplate.cxx.txt | sed 's/[O]perationName/OperationCiftiEstimateFWHM/g' | sed 's/-[c]ommand-switch/-cifti-estimate-fwhm/g' | sed 's|[S]HORT DESCRIPTION|ESTIMATE FWHM SMOOTHNESS OF A CIFTI FILE|g' > src/Operations/OperationCiftiEstimateFWHM.cxx
-
-or manually copy and replace
-
-next, implement its functions
-
-add these to Operations/CMakeLists.txt:
-
-OperationCiftiEstimateFWHM.h
-OperationCiftiEstimateFWHM.cxx
-
-place the following lines into Commands/CommandOperationManager.cxx:
-
-#include "OperationCiftiEstimateFWHM.h"
-    //near the top
-
-    this->commandOperations.push_back(new CommandParser(new AutoOperationCiftiEstimateFWHM()));
-        //in CommandOperationManager()
-
-finally, remove this block comment
-*/
-
 #include "AbstractOperation.h"
 
 namespace caret {
