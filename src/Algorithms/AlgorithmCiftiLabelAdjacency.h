7dc894bdc80bd1dd4c4dcb3ac1c2856af4c2b139
Tim Coalson <tsc5yc@mst.edu>
2014-04-11 17:52:34 -0500

    remove instruction comment on new command

--- src/Algorithms/AlgorithmCiftiLabelAdjacency.h
+++ src/Algorithms/AlgorithmCiftiLabelAdjacency.h
@@ -21,42 +21,6 @@
  */
 /*LICENSE_END*/
 
-/*
-file->save as... and enter what you will name the class, plus .h
-
-find and replace these strings, without matching "whole word only" (plain text mode):
-
-AlgorithmCiftiLabelAdjacency     : algorithm name, in CamelCase, with initial capital, same as what you saved the header file to
-ALGORITHM_CIFTI_LABEL_ADJACENCY    : uppercase of algorithm name, with underscore between words, used in #ifdef guards
--cifti-label-adjacency   : switch for the command line to use, often hyphenated version of algorithm name, lowercase, minus "algorithm"
-MAKE ADJACENCY MATRIX OF A CIFTI LABEL FILE : short description of the command, uppercase, three to five words, often just command switch with more verbosity
-
-next, make AlgorithmCiftiLabelAdjacency.cxx from AlgorithmTemplate.cxx.txt via one of the following (depending on working directory):
-
-cat AlgorithmTemplate.cxx.txt | sed 's/[A]lgorithmName/AlgorithmCiftiLabelAdjacency/g' | sed 's/-[c]ommand-switch/-cifti-label-adjacency/g' | sed 's/[S]HORT DESCRIPTION/MAKE ADJACENCY MATRIX OF A CIFTI LABEL FILE/g' > AlgorithmCiftiLabelAdjacency.cxx
-cat Algorithms/AlgorithmTemplate.cxx.txt | sed 's/[A]lgorithmName/AlgorithmCiftiLabelAdjacency/g' | sed 's/-[c]ommand-switch/-cifti-label-adjacency/g' | sed 's/[S]HORT DESCRIPTION/MAKE ADJACENCY MATRIX OF A CIFTI LABEL FILE/g' > Algorithms/AlgorithmCiftiLabelAdjacency.cxx
-cat src/Algorithms/AlgorithmTemplate.cxx.txt | sed 's/[A]lgorithmName/AlgorithmCiftiLabelAdjacency/g' | sed 's/-[c]ommand-switch/-cifti-label-adjacency/g' | sed 's|[S]HORT DESCRIPTION|MAKE ADJACENCY MATRIX OF A CIFTI LABEL FILE|g' > src/Algorithms/AlgorithmCiftiLabelAdjacency.cxx
-
-or manually copy and replace
-
-next, implement its functions - the algorithm work goes in the CONSTRUCTOR
-
-add these into Algorithms/CMakeLists.txt:
-
-AlgorithmCiftiLabelAdjacency.h
-AlgorithmCiftiLabelAdjacency.cxx
-
-place the following lines into Commands/CommandOperationManager.cxx:
-
-#include "AlgorithmCiftiLabelAdjacency.h"
-    //near the top
-
-    this->commandOperations.push_back(new CommandParser(new AutoAlgorithmCiftiLabelAdjacency()));
-        //in CommandOperationManager()
-
-finally, remove this block comment
-*/
-
 #include "AbstractAlgorithm.h"
 
 namespace caret {
