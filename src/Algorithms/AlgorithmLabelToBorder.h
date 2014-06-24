7b37b4dcac2259725e7e0a0fdb8a802319957950
Tim Coalson <tsc5yc@mst.edu>
2014-06-23 21:48:44 -0500

    remove instruction comment in new command

--- src/Algorithms/AlgorithmLabelToBorder.h
+++ src/Algorithms/AlgorithmLabelToBorder.h
@@ -21,42 +21,6 @@
  */
 /*LICENSE_END*/
 
-/*
-file->save as... and enter what you will name the class, plus .h
-
-find and replace these strings, without matching "whole word only" (plain text mode):
-
-AlgorithmLabelToBorder     : algorithm name, in CamelCase, with initial capital, same as what you saved the header file to
-ALGORITHM_LABEL_TO_BORDER    : uppercase of algorithm name, with underscore between words, used in #ifdef guards
--label-to-border   : switch for the command line to use, often hyphenated version of algorithm name, lowercase, minus "algorithm"
-DRAW BORDERS AROUND LABELS : short description of the command, uppercase, three to five words, often just command switch with more verbosity
-
-next, make AlgorithmLabelToBorder.cxx from AlgorithmTemplate.cxx.txt via one of the following (depending on working directory):
-
-cat AlgorithmTemplate.cxx.txt | sed 's/[A]lgorithmName/AlgorithmLabelToBorder/g' | sed 's/-[c]ommand-switch/-label-to-border/g' | sed 's/[S]HORT DESCRIPTION/DRAW BORDERS AROUND LABELS/g' > AlgorithmLabelToBorder.cxx
-cat Algorithms/AlgorithmTemplate.cxx.txt | sed 's/[A]lgorithmName/AlgorithmLabelToBorder/g' | sed 's/-[c]ommand-switch/-label-to-border/g' | sed 's/[S]HORT DESCRIPTION/DRAW BORDERS AROUND LABELS/g' > Algorithms/AlgorithmLabelToBorder.cxx
-cat src/Algorithms/AlgorithmTemplate.cxx.txt | sed 's/[A]lgorithmName/AlgorithmLabelToBorder/g' | sed 's/-[c]ommand-switch/-label-to-border/g' | sed 's|[S]HORT DESCRIPTION|DRAW BORDERS AROUND LABELS|g' > src/Algorithms/AlgorithmLabelToBorder.cxx
-
-or manually copy and replace
-
-next, implement its functions - the algorithm work goes in the CONSTRUCTOR
-
-add these into Algorithms/CMakeLists.txt:
-
-AlgorithmLabelToBorder.h
-AlgorithmLabelToBorder.cxx
-
-place the following lines into Commands/CommandOperationManager.cxx:
-
-#include "AlgorithmLabelToBorder.h"
-    //near the top
-
-    this->commandOperations.push_back(new CommandParser(new AutoAlgorithmLabelToBorder()));
-        //in CommandOperationManager()
-
-finally, remove this block comment
-*/
-
 #include "AbstractAlgorithm.h"
 
 namespace caret {
