b7233e7deb4b3a7d1063741eea37b656489d7e75
John Harwell <john@brainmap.wustl.edu>
2014-07-22 11:55:03 -0500

    Update windows 64 bit build for newer freetype library.

--- build_scripts/windows64/build.bat
+++ build_scripts/windows64/build.bat
@@ -5,7 +5,8 @@ set OLD_DIR=%CD%
 REM rd /q/s %DIR%\caret7_source\build
 md %DIR%\caret7_source\build
 cd /d %DIR%\caret7_source\build
-cmake -DFREETYPE_LIBRARY=C:\dev64\install\FreeType\lib\freetype240MT_D.lib -G "NMake Makefiles JOM" -DCMAKE_BUILD_TYPE=Release ../src
+REM cmake -DFREETYPE_LIBRARY=C:\dev64\install\FreeType\lib\freetype240MT_D.lib -G "NMake Makefiles JOM" -DCMAKE_BUILD_TYPE=Release ../src
+cmake -DFREETYPE_INCLUDE_DIR_freetype2=C:\dev64\install\FreeType-2.4.8\include\freetype2 -DFREETYPE_INCLUDE_DIR_ft2build=C:\dev64\install\FreeType-2.4.8\include  -DFREETYPE_LIBRARY=C:\dev64\install\FreeType-2.4.8\lib\freetype.lib -G "NMake Makefiles JOM" -DCMAKE_BUILD_TYPE=Release ../src
 jom -j8
 cd /d %OLD_DIR%
 set OLD_DIR=
