446835048e7c9a60347f4cdc9b96e33555506b1e
John Harwell <john@brainmap.wustl.edu>
2014-07-23 09:30:38 -0500

    Updated windows 32 build for FreeType

--- build_scripts/windows32/build.bat
+++ build_scripts/windows32/build.bat
@@ -5,7 +5,8 @@ set OLD_DIR=%CD%
 REM rd /q/s %DIR%\caret7_source\build
 md %DIR%\caret7_source\build
 cd /d %DIR%\caret7_source\build
-cmake -G "NMake Makefiles JOM" -DCMAKE_BUILD_TYPE=Release ../src
+REMcmake -G "NMake Makefiles JOM" -DCMAKE_BUILD_TYPE=Release ../src 
+cmake -DFREETYPE_INCLUDE_DIR_freetype2=C:\dev32\install\FreeType-2.4.8\include\freetype2 -DFREETYPE_INCLUDE_DIR_ft2build=C:\dev32\install\FreeType-2.4.8\include  -DFREETYPE_LIBRARY=C:\dev32\install\FreeType-2.4.8\lib\freetype.lib -G "NMake Makefiles JOM" -DCMAKE_BUILD_TYPE=Release ../src
 jom -j8
 cd /d %OLD_DIR%
 set OLD_DIR=
