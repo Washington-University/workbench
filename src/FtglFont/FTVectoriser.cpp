2437e5c76f8ce26a20f5fc3a8a7ee1e4be612d89
John Harwell <john@brainmap.wustl.edu>
2014-07-29 09:19:42 -0500

    Fix for FTGL so that is compiles on Mac with GCC-4.9.0

--- src/FtglFont/FTVectoriser.cpp
+++ src/FtglFont/FTVectoriser.cpp
@@ -34,7 +34,14 @@
 #define CALLBACK
 #endif
 
-#if defined __APPLE_CC__ && __APPLE_CC__ < 5465
+#define GCC_VERSION (__GNUC__ * 10000 \
+                     + __GNUC_MINOR__ * 100 \
+                     + __GNUC_PATCHLEVEL__)
+
+/* Test for GCC > 4.9.0 on Apple  JWH */
+#if defined __APPLE_CC__ && GCC_VERSION > 4900
+    typedef GLvoid (*GLUTesselatorFunction) ();
+#elif defined __APPLE_CC__ && __APPLE_CC__ < 5465
     typedef GLvoid (*GLUTesselatorFunction) (...);
 #elif defined WIN32 && !defined __CYGWIN__
     typedef GLvoid (CALLBACK *GLUTesselatorFunction) ();
