335eec24982ebc0595b15d4b07e39fa1140f08b3
John Harwell <john@brainmap.wustl.edu>
2014-08-15 16:03:41 -0500

    Combined the two scripts for updating resources with new help files 'update_help_files.sh' and creating the resources 'create_resources_qrc_file.h' into a one script named 'update_resources.sh'.

--- src/Resources/create_resources_qrc_file.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rcc -project -o resources.qrc
-
-echo "You will need to delete the Desktop and CommandLine projects in the build directories, run cmake, and then compile."
---
+++ src/Resources/update_resources.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+mv HelpFiles OLD_HelpFiles
+
+cp -r /mnt/myelin/shared/WB_Tutorial/WB_1.0_Help ./HelpFiles
+
+rm -rf OLD_HelpFiles
+
+rcc -project -o resources.qrc
+
+echo ""
+echo "You now need to:"
+echo "   1) Run CMake in the build directory"
+echo "   2) Compile"
+echo "   3) Commit changes to GIT repository"
+echo "   4) Push source code to master repository"
+echo ""
