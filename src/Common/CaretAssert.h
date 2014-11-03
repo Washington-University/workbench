4290806975cfbe5417660c3246fd493b4243bf90
John Harwell <john@brainmap.wustl.edu>
2014-11-03 08:05:21 -0600

    the CaretAssertToDo was not removed when compiling optimized.

--- src/Common/CaretAssert.h
+++ src/Common/CaretAssert.h
@@ -28,7 +28,8 @@
 #ifdef NDEBUG
 
     #define CaretAssert(e) ((void)0)
-    #define CaretAssertToDo(e) ((void)0)
+    #define CaretAssertToDoWarning() ((void)0)
+    #define CaretAssertToDoFatal() ((void)0)
     #define CaretAssertMessage(e, m) ((void)0)
     #define CaretAssertArrayIndex(a, n, i) ((void) 0)
     #define CaretAssertVectorIndex(v, i) ((void) 0)
