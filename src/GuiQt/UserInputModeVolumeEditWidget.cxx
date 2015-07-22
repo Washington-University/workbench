51913fc2d71b6176bb343b22074bd5143d3a6c0b
John Harwell <john@brainmap.wustl.edu>
2015-07-22 08:56:57 -0500

    Disabling part of volume toolbar for testing with WB-538.

--- src/GuiQt/UserInputModeVolumeEditWidget.cxx
+++ src/GuiQt/UserInputModeVolumeEditWidget.cxx
@@ -83,7 +83,7 @@ m_windowIndex(windowIndex)
     
     QVBoxLayout* layout = new QVBoxLayout(this);
     WuQtUtilities::setLayoutSpacingAndMargins(layout, 2, 0);
-    layout->addWidget(createSelectionToolBar());
+//    layout->addWidget(createSelectionToolBar());
     layout->addWidget(createModeToolBar());
     setSizePolicy(sizePolicy().horizontalPolicy(), QSizePolicy::Fixed);
     
@@ -284,14 +284,14 @@ UserInputModeVolumeEditWidget::createSelectionToolBar()
     
     m_yBrushSizeSpinBox = new WuQSpinBoxOddValue(this);
     m_yBrushSizeSpinBox->setRange(MIN_BRUSH_SIZE, MAX_BRUSH_SIZE);
-    QObject::connect(m_xBrushSizeSpinBox, SIGNAL(valueChanged(int)),
+    QObject::connect(m_yBrushSizeSpinBox, SIGNAL(valueChanged(int)),
                      this, SLOT(yBrushSizeValueChanged(int)));
     m_yBrushSizeSpinBox->getWidget()->setToolTip("Coronal brush size (voxels).\n"
                                                  "Must be an odd value.");
     
     m_zBrushSizeSpinBox = new WuQSpinBoxOddValue(this);
     m_zBrushSizeSpinBox->setRange(MIN_BRUSH_SIZE, MAX_BRUSH_SIZE);
-    QObject::connect(m_xBrushSizeSpinBox, SIGNAL(valueChanged(int)),
+    QObject::connect(m_zBrushSizeSpinBox, SIGNAL(valueChanged(int)),
                      this, SLOT(zBrushSizeValueChanged(int)));
     m_zBrushSizeSpinBox->getWidget()->setToolTip("Axial brush size (voxels).\n"
                                                  "Must be an odd value.");
