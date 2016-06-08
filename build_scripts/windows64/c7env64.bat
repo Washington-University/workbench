f98dba836d738239679510bb46370149e9f55491
John Harwell <john@brainmap.wustl.edu>
2016-06-08 11:57:08 -0500

    Changed windows 64 build to use newly build version of Qt, 4.8.6

--- build_scripts/windows64/c7env64.bat
+++ build_scripts/windows64/c7env64.bat
@@ -1,5 +1,6 @@
 set PATH=C:\dev64\install\Qt481\bin;C:\Qt\qtcreator-2.4.1\bin;%PATH%
 set QTDIR=C:\dev64\install\Qt481
+set QTDIR=C:\dev64\install\qt-4.8.6
 set ZLIB_LIB_DIR=C:\dev64\install\zlib\lib
 set ZLIB_INC_DIR=C:\dev64\install\zlib\include
 set FREETYPE_DIR=C:\dev64\install\FreeType
