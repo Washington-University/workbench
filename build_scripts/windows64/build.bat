d90f55ddc119eb215814ff0e78d0a24e96ac0424
John Harwell <john@brainmap.wustl.edu>
2016-06-14 14:48:50 -0500

    Fixes for SSL on windows including new version of SSL, correct path for Qt, new version of Qt (4.8.7), and a new version of CMake that correctly recognizes OpenSSL.

--- build_scripts/windows64/build.bat
+++ build_scripts/windows64/build.bat
@@ -2,11 +2,16 @@
 if [%MACHINE%]==[] call setup_env.bat
 
 set OLD_DIR=%CD%
-REM rd /q/s %DIR%\caret7_source\build
 md %DIR%\caret7_source\build
 cd /d %DIR%\caret7_source\build
-REM cmake -DFREETYPE_LIBRARY=C:\dev64\install\FreeType\lib\freetype240MT_D.lib -G "NMake Makefiles JOM" -DCMAKE_BUILD_TYPE=Release ../src
-cmake -DFREETYPE_INCLUDE_DIR_freetype2=C:\dev64\install\FreeType-2.4.8\include\freetype2 -DFREETYPE_INCLUDE_DIR_ft2build=C:\dev64\install\FreeType-2.4.8\include  -DFREETYPE_LIBRARY=C:\dev64\install\FreeType-2.4.8\lib\freetype.lib -G "NMake Makefiles JOM" -DCMAKE_BUILD_TYPE=Release ../src
+cmake ^
+   -DFREETYPE_INCLUDE_DIR_freetype2=C:\dev64\install\FreeType-2.4.8\include\freetype2 ^
+   -DFREETYPE_INCLUDE_DIR_ft2build=C:\dev64\install\FreeType-2.4.8\include ^
+   -DFREETYPE_LIBRARY=C:\dev64\install\FreeType-2.4.8\lib\freetype.lib ^
+   -G "NMake Makefiles JOM" ^
+   -DCMAKE_BUILD_TYPE=Release ^
+   ../src
+
 jom -j8
 cd /d %OLD_DIR%
 set OLD_DIR=
--- build_scripts/windows64/c7env64.bat
+++ build_scripts/windows64/c7env64.bat
@@ -1,7 +1,7 @@
 REM set PATH=C:\dev64\install\Qt481\bin;C:\Qt\qtcreator-2.4.1\bin;%PATH%
 REM set QTDIR=C:\dev64\install\Qt481
-set QTDIR=C:\dev64\install\qt-4.8.6
+set QTDIR=C:\dev64\install\qt-4.8.7
 set PATH=%QTDIR%\bin;C:\Qt\qtcreator-2.4.1\bin;%PATH%
 set ZLIB_LIB_DIR=C:\dev64\install\zlib\lib
 set ZLIB_INC_DIR=C:\dev64\install\zlib\include
-set FREETYPE_DIR=C:\dev64\install\FreeType
+set FREETYPE_DIR=C:\dev64\install\FreeType-2.4.8
