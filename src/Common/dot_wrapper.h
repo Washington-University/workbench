b4b76bf7d853d65196986d6ae0b7ce2d82db1207
Tim Coalson <tsc5yc@mst.edu>
2016-10-10 17:30:40 -0500

    update copyrights, extra stubs for compiling SIMD option code without testing SIMD support defines

--- src/Common/dot_wrapper.h
+++ src/Common/dot_wrapper.h
@@ -20,6 +20,19 @@ inline double sddot (const float *a, const float *b, int n)
     sum += a[k] * b[k];
   return sum;
 }  // sddot()
+//copy enum from dot.h
+enum flags {
+    DOT_NAIVE  = 1,
+    DOT_SSE2   = 2,
+    DOT_AVX    = 3,
+    DOT_AVXFMA = 4,
+    DOT_AUTO   = 100
+};
+//and dummy implementation of dot_set_impl
+inline int dot_set_impl (int)
+{
+    return DOT_NAIVE;
+}
 #endif
 
 #ifdef __cplusplus
