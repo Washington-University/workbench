163439885aa2555ad00c435d464aa7a67a5a8b3d
John Harwell <john@brainmap.wustl.edu>
2016-11-09 14:36:58 -0600

    Modified src/CMakeLists.txt so that the paths to ZLIB are not hardcoded on Windows and instead set the variable 'ZLIB_ROOT' used by the FindZLIB module in CMake.  Modified the (older) build scripts for windows so that they set ZLIB_ROOT as a parameter to the 'cmake' command.  In addition, the variable WORKBENCH_FREETYPE_DIR has been added to specify the path to the FreeType root directory.  If this variable is present, it is used by CMake to set the environment variable FREETYPE_DIR used in the CMake module FindFreeType.  Lastly, added CMake Policy CMP0020 to CMakeLists.txt for Windows only to eliminate many warnings produced by newer versions of CMake.

--- build_scripts/windows32/build.bat
+++ build_scripts/windows32/build.bat
@@ -6,7 +6,18 @@ REM rd /q/s %DIR%\caret7_source\build
 md %DIR%\caret7_source\build
 cd /d %DIR%\caret7_source\build
 REMcmake -G "NMake Makefiles JOM" -DCMAKE_BUILD_TYPE=Release ../src 
-cmake -DFREETYPE_INCLUDE_DIR_freetype2=C:\dev32\install\FreeType-2.4.8\include\freetype2 -DFREETYPE_INCLUDE_DIR_ft2build=C:\dev32\install\FreeType-2.4.8\include  -DFREETYPE_LIBRARY=C:\dev32\install\FreeType-2.4.8\lib\freetype.lib -G "NMake Makefiles JOM" -DCMAKE_BUILD_TYPE=Release ../src
+
+cmake ^
+   -DWORKBENCH_FREETYPE_DIR=C:\dev32\install\FreeType-2.4.8\lib ^
+   -DZLIB_ROOT=C:\dev32\install\zlib ^
+    -G "NMake Makefiles JOM" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    ../src
+
 jom -j8
 cd /d %OLD_DIR%
 set OLD_DIR=
+
+#JWH   -DFREETYPE_INCLUDE_DIR_freetype2=C:\dev32\install\FreeType-2.4.8\include\freetype2 
+#JWH   -DFREETYPE_INCLUDE_DIR_ft2build=C:\dev32\install\FreeType-2.4.8\include
+#JWH   -DFREETYPE_LIBRARY=C:\dev32\install\FreeType-2.4.8\lib\freetype.lib
--- build_scripts/windows64/build.bat
+++ build_scripts/windows64/build.bat
@@ -5,9 +5,8 @@ set OLD_DIR=%CD%
 md %DIR%\caret7_source\build
 cd /d %DIR%\caret7_source\build
 cmake ^
-   -DFREETYPE_INCLUDE_DIR_freetype2=C:\dev64\install\FreeType-2.4.8\include\freetype2 ^
-   -DFREETYPE_INCLUDE_DIR_ft2build=C:\dev64\install\FreeType-2.4.8\include ^
-   -DFREETYPE_LIBRARY=C:\dev64\install\FreeType-2.4.8\lib\freetype.lib ^
+   -DWORKBENCH_FREETYPE_DIR=C:\dev64\install\FreeType-2.4.8 ^
+   -DZLIB_ROOT=C:\dev64\install\zlib ^
    -G "NMake Makefiles JOM" ^
    -DCMAKE_BUILD_TYPE=Release ^
    ../src
@@ -15,3 +14,8 @@ cmake ^
 jom -j8
 cd /d %OLD_DIR%
 set OLD_DIR=
+
+
+REM JWH   -DFREETYPE_INCLUDE_DIR_freetype2=C:\dev64\install\FreeType-2.4.8\include\freetype2
+REM JWH   -DFREETYPE_INCLUDE_DIR_ft2build=C:\dev64\install\FreeType-2.4.8\include
+REM JWH   -DFREETYPE_LIBRARY=C:\dev64\install\FreeType-2.4.8\lib\freetype.lib
