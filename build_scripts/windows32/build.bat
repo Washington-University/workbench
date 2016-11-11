092c5faf7afd2d96dc3f40084b34120893f70563
John Harwell <john@brainmap.wustl.edu>
2016-11-11 15:33:10 -0600

    Fixed improperly commented out code in build_scripts/windows32/build.bat

--- build_scripts/windows32/build.bat
+++ build_scripts/windows32/build.bat
@@ -7,6 +7,11 @@ md %DIR%\caret7_source\build
 cd /d %DIR%\caret7_source\build
 REMcmake -G "NMake Makefiles JOM" -DCMAKE_BUILD_TYPE=Release ../src 
 
+REM   -DFREETYPE_INCLUDE_DIR_freetype2=C:\dev32\install\FreeType-2.4.8\include\freetype2 
+REM   -DFREETYPE_INCLUDE_DIR_ft2build=C:\dev32\install\FreeType-2.4.8\include
+REM   -DFREETYPE_LIBRARY=C:\dev32\install\FreeType-2.4.8\lib\freetype.lib
+
+
 cmake ^
    -DWORKBENCH_FREETYPE_DIR=C:\dev32\install\FreeType-2.4.8\lib ^
    -DZLIB_ROOT=C:\dev32\install\zlib ^
@@ -18,6 +23,3 @@ jom -j8
 cd /d %OLD_DIR%
 set OLD_DIR=
 
-#JWH   -DFREETYPE_INCLUDE_DIR_freetype2=C:\dev32\install\FreeType-2.4.8\include\freetype2 
-#JWH   -DFREETYPE_INCLUDE_DIR_ft2build=C:\dev32\install\FreeType-2.4.8\include
-#JWH   -DFREETYPE_LIBRARY=C:\dev32\install\FreeType-2.4.8\lib\freetype.lib
