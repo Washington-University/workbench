7d5fb46b4fa4501c87dfec9e87bae7a01e7a87ca
John Harwell <john@brainmap.wustl.edu>
2016-12-08 15:23:24 -0600

    Redhat remote build script was replacing the build result file when copying executables instead of appending to the build log file.

--- build_scripts/linux64_rh_remote.sh
+++ build_scripts/linux64_rh_remote.sh
@@ -18,8 +18,8 @@ if [ ! $result ]; then
     echo "COPYING PROGRAMS"
     COPY_FROM_DIR=caret@${COPY_SERVER}:${BUILD_DIR}/build
     DIST_DIR=/mainpool/storage/distribution/caret7_distribution/workbench/exe_rh_linux64
-    scp ${COPY_FROM_DIR}/Desktop/wb_view ${DIST_DIR} > $PWD/remote_launch_${BUILD}.txt 2>&1
-    scp ${COPY_FROM_DIR}/CommandLine/wb_command ${DIST_DIR} > $PWD/remote_launch_${BUILD}.txt 2>&1
+    scp ${COPY_FROM_DIR}/Desktop/wb_view ${DIST_DIR} >> $PWD/remote_launch_${BUILD}.txt 2>&1
+    scp ${COPY_FROM_DIR}/CommandLine/wb_command ${DIST_DIR} >> $PWD/remote_launch_${BUILD}.txt 2>&1
  
     # 
     # Need to be changed
@@ -28,11 +28,11 @@ if [ ! $result ]; then
     #put the completion into the exe directory, because it isn't useful to run it directly (and would make wb_comm<tab> behave worse)
     #scp ${GIT_ROOT_DIR}/src/CommandLine/bashcomplete_wb_command caret@myelin1.wustl.edu:${DIST_DIR}
 
-echo "SCRIPT COMPLETED SUCCESSFULLY"
+    echo "SCRIPT COMPLETED SUCCESSFULLY"
 else
     echo "Build failed"
 fi
 
-cat $PWD/remote_launch_${BUILD}.txt | mailx -s 'Caret7 Linux 64 RedHat Build Result' john@brainvis.wustl.edu tsc5yc@mst.edu
+cat $PWD/remote_launch_${BUILD}.txt | mailx -v -s 'Caret7 Linux 64 RedHat Build Result' john@brainvis.wustl.edu tsc5yc@mst.edu
 
 
