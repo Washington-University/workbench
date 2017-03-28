7be557c2304e2f306f9091c32308e6351c8ca1fd
Tim Coalson <tsc5yc@mst.edu>
2017-03-27 23:21:29 -0500

    add option to output separate dconns per fiber in -convert-matrix4-to-matrix2

--- src/Operations/OperationConvertMatrix4ToMatrix2.cxx
+++ src/Operations/OperationConvertMatrix4ToMatrix2.cxx
@@ -47,8 +47,14 @@ OperationParameters* OperationConvertMatrix4ToMatrix2::getParameters()
     OptionalParameter* distanceOpt = ret->createOptionalParameter(3, "-distances", "output average trajectory distance");
     distanceOpt->addCiftiOutputParameter(1, "distance-out", "the distances, as a cifti file");
     
+    OptionalParameter* fibersOpt = ret->createOptionalParameter(4, "-individual-fibers", "output files for each fiber direction");
+    fibersOpt->addCiftiOutputParameter(1, "fiber-1", "output file for first fiber");
+    fibersOpt->addCiftiOutputParameter(2, "fiber-2", "output file for second fiber");
+    fibersOpt->addCiftiOutputParameter(3, "fiber-3", "output file for third fiber");
+    
     ret->setHelpText(
-        AString("This command makes a cifti file from the fiber counts in a matrix4 wbsparse file, and optionally a second cifti file from the distances.")
+        AString("This command makes a cifti file from the fiber counts in a matrix4 wbsparse file, and optionally a second cifti file from the distances.  ") +
+        "Note that while the total count is stored exactly, the per-fiber counts are stored as approximate fractions, so the output of -individual-fibers will contain nonintegers."
     );
     return ret;
 }
@@ -64,23 +70,51 @@ void OperationConvertMatrix4ToMatrix2::useParameters(OperationParameters* myPara
     {
         myDistOut = distanceOpt->getOutputCifti(1);
     }
+    CiftiFile* fiber1 = NULL, *fiber2 = NULL, *fiber3 = NULL;
+    OptionalParameter* fibersOpt = myParams->getOptionalParameter(4);
+    if (fibersOpt->m_present)
+    {
+        fiber1 = fibersOpt->getOutputCifti(1);
+        fiber2 = fibersOpt->getOutputCifti(2);
+        fiber3 = fibersOpt->getOutputCifti(3);
+    }
     CaretSparseFile matrix4(matrix4Name);
     const CiftiXML& myXML = matrix4.getCiftiXML();
     myCountsOut->setCiftiXML(myXML);
     if (myDistOut != NULL) myDistOut->setCiftiXML(myXML);
+    if (fiber1 != NULL)
+    {
+        fiber1->setCiftiXML(myXML);
+        fiber2->setCiftiXML(myXML);
+        fiber3->setCiftiXML(myXML);
+    }
     int rowSize = myXML.getDimensionLength(CiftiXML::ALONG_ROW), numRows = myXML.getDimensionLength(CiftiXML::ALONG_COLUMN);
-    vector<float> scratchRow(rowSize, 0.0f);
-    vector<int64_t> indices;
-    vector<FiberFractions> fibers;
     for (int i = 0; i < numRows; ++i)
     {
+        vector<float> scratchRow(rowSize, 0.0f);
+        vector<float> scratchFiber1 = scratchRow, scratchFiber2 = scratchRow, scratchFiber3 = scratchRow;
+        vector<int64_t> indices;
+        vector<FiberFractions> fibers;
         matrix4.getFibersRowSparse(i, indices, fibers);
         int numIndices = (int)indices.size();
         for (int j = 0; j < numIndices; ++j)
         {
             scratchRow[indices[j]] = fibers[j].totalCount;
+            if (fiber1 != NULL)
+            {
+                CaretAssert(fibers[j].fiberFractions.size() == 3);
+                scratchFiber1[indices[j]] = fibers[j].totalCount * fibers[j].fiberFractions[0];
+                scratchFiber2[indices[j]] = fibers[j].totalCount * fibers[j].fiberFractions[1];
+                scratchFiber3[indices[j]] = fibers[j].totalCount * fibers[j].fiberFractions[2];
+            }
         }
         myCountsOut->setRow(scratchRow.data(), i);
+        if (fiber1 != NULL)
+        {
+            fiber1->setRow(scratchFiber1.data(), i);
+            fiber2->setRow(scratchFiber2.data(), i);
+            fiber3->setRow(scratchFiber3.data(), i);
+        }
         if (myDistOut != NULL)
         {
             for (int j = 0; j < numIndices; ++j)
@@ -88,10 +122,6 @@ void OperationConvertMatrix4ToMatrix2::useParameters(OperationParameters* myPara
                 scratchRow[indices[j]] = fibers[j].distance;
             }
             myDistOut->setRow(scratchRow.data(), i);
-        }
-        for (int j = 0; j < numIndices; ++j)
-        {
-            scratchRow[indices[j]] = 0.0f;
         }
     }
 }
