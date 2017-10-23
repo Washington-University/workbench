ff7f152c38a0938ffdae9e5dc21598d94551829a
John Harwell <john@brainmap.wustl.edu>
2017-10-23 15:02:03 -0500

    Added JsonHelper class to assist with processing of JSON.

---
+++ src/Common/JsonHelper.cxx
@@ -0,0 +1,113 @@
+
+/*LICENSE_START*/
+/*
+ *  Copyright (C) 2017 Washington University School of Medicine
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+/*LICENSE_END*/
+
+#define __JSON_HELPER_DECLARE__
+#include "JsonHelper.h"
+#undef __JSON_HELPER_DECLARE__
+
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonValue>
+
+#include "CaretAssert.h"
+using namespace caret;
+
+
+    
+/**
+ * \class caret::JsonHelper 
+ * \brief Helper class for JSON processing.
+ * \ingroup Common
+ */
+
+/**
+ * Constructor.
+ */
+JsonHelper::JsonHelper()
+{
+    
+}
+
+/**
+ * Destructor.
+ */
+JsonHelper::~JsonHelper()
+{
+}
+
+/**
+ * Determine type of content in the value and return its string representation
+ *
+ * @param jsonValue
+ *     The JSON Value
+ * @return
+ *     The string representation of the value.
+ */
+AString
+JsonHelper::jsonValueToString(const QJsonValue& jsonValue)
+{
+    AString typeString("Invalid: ");
+    AString contentString;
+    QJsonDocument contentJson;
+    
+    switch (jsonValue.type()) {
+        case QJsonValue::Array:
+            typeString = "Array: ";
+            contentJson = QJsonDocument(jsonValue.toArray());
+            break;
+        case QJsonValue::Bool:
+            typeString = "Bool: ";
+            contentString = AString::fromBool(jsonValue.toBool());
+            break;
+        case QJsonValue::Double:
+            typeString = "Double: ";
+            contentString = AString::fromBool(jsonValue.toDouble());
+            break;
+        case QJsonValue::Null:
+            typeString = "Null: ";
+            contentString = "";
+            break;
+        case QJsonValue::Object:
+            typeString = "Object: ";
+            contentJson = QJsonDocument(jsonValue.toObject());
+            break;
+        case QJsonValue::String:
+            typeString = "String: ";
+            contentString = jsonValue.toString();
+            break;
+        case QJsonValue::Undefined:
+            typeString = "Undefined: ";
+            contentString = "";
+            break;
+    }
+    
+    AString s(typeString);
+    
+    if (contentJson.isNull()) {
+        s.append(contentString);
+    }
+    else {
+        s.append(contentJson.toJson(QJsonDocument::Compact));
+    }
+    
+    return s;
+}
---
+++ src/Common/JsonHelper.h
@@ -0,0 +1,61 @@
+#ifndef __JSON_HELPER_H__
+#define __JSON_HELPER_H__
+
+/*LICENSE_START*/
+/*
+ *  Copyright (C) 2017 Washington University School of Medicine
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+/*LICENSE_END*/
+
+
+
+#include <memory>
+
+#include "AString.h"
+
+class QJsonValue;
+
+
+namespace caret {
+
+    class JsonHelper {
+        
+    public:
+        static AString jsonValueToString(const QJsonValue& jsonValue);
+
+
+        // ADD_NEW_METHODS_HERE
+
+    private:
+        JsonHelper();
+        
+        virtual ~JsonHelper();
+        
+        JsonHelper(const JsonHelper&);
+
+        JsonHelper& operator=(const JsonHelper&);
+        
+        // ADD_NEW_MEMBERS_HERE
+
+    };
+    
+#ifdef __JSON_HELPER_DECLARE__
+    // <PLACE DECLARATIONS OF STATIC MEMBERS HERE>
+#endif // __JSON_HELPER_DECLARE__
+
+} // namespace
+#endif  //__JSON_HELPER_H__
