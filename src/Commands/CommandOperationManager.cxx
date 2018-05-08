625135bfb16edcf187a571d8ee64b8bcc7bf9d58
Tim Coalson <tsc5yc@mst.edu>
2018-05-08 18:17:54 -0500

    add missing information switches to completion

--- src/Commands/CommandOperationManager.cxx
+++ src/Commands/CommandOperationManager.cxx
@@ -721,7 +721,7 @@ AString CommandOperationManager::doCompletion(ProgramParameters& parameters, con
     const uint64_t numberOfDeprecated = this->deprecatedOperations.size();
     if (!parameters.hasNext())
     {//suggest all commands, including deprecated and informational (order doesn't matter, bash sorts them before displaying)
-        ret += "\\ -help\\ -arguments-help\\ -cifti-help\\ -gifti-help\\ -parallel-help\\ -version\\ -list-commands\\ -list-deprecated-commands\\ -all-commands-help";
+        ret += "\\ -help\\ -arguments-help\\ -global-options\\ -parallel-help\\ -cifti-help\\ -gifti-help\\ -parallel-help\\ -version\\ -list-commands\\ -list-deprecated-commands\\ -all-commands-help";
         for (uint64_t i = 0; i < numberOfCommands; i++)
         {
             ret += "\\ " + commandOperations[i]->getCommandLineSwitch();
