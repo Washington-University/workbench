ffee494c37fdd8fb76f7bfd42941a6dcdd8bf568
John Harwell <jharwell@wustl.edu>
2019-06-12 12:54:03 -0500

    Added WuQSpinBox that emits a return pressed signal for a QSpinBox.

---
+++ src/GuiQt/WuQSpinBox.cxx
@@ -0,0 +1,66 @@
+
+/*LICENSE_START*/
+/*
+ *  Copyright (C) 2019 Washington University School of Medicine
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+/*LICENSE_END*/
+
+#define __WU_Q_SPIN_BOX_DECLARE__
+#include "WuQSpinBox.h"
+#undef __WU_Q_SPIN_BOX_DECLARE__
+
+#include <QKeyEvent>
+
+#include "CaretAssert.h"
+
+using namespace caret;
+
+#include <iostream>
+
+    
+/**
+ * \class caret::WuQSpinBox 
+ * \brief Emits a signal when the return key is pressed
+ * \ingroup GuiQt
+ */
+
+/**
+ * Constructor.
+ */
+WuQSpinBox::WuQSpinBox()
+: QSpinBox()
+{
+}
+
+/**
+ * Destructor.
+ */
+WuQSpinBox::~WuQSpinBox()
+{
+}
+
+void
+WuQSpinBox::keyPressEvent(QKeyEvent* event)
+{
+    if (event->key() == Qt::Key_Return) {
+        std::cout << "Return key pressed";
+        emit signalReturnPressed();
+    }
+    
+    QSpinBox::keyPressEvent(event);
+}
+
---
+++ src/GuiQt/WuQSpinBox.h
@@ -0,0 +1,66 @@
+#ifndef __WU_Q_SPIN_BOX_H__
+#define __WU_Q_SPIN_BOX_H__
+
+/*LICENSE_START*/
+/*
+ *  Copyright (C) 2019 Washington University School of Medicine
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+/*LICENSE_END*/
+
+
+
+#include <memory>
+
+#include <QSpinBox>
+
+
+
+namespace caret {
+
+    class WuQSpinBox : public QSpinBox {
+        
+        Q_OBJECT
+
+    public:
+        WuQSpinBox();
+        
+        virtual ~WuQSpinBox();
+        
+        WuQSpinBox(const WuQSpinBox&) = delete;
+
+        WuQSpinBox& operator=(const WuQSpinBox&) = delete;
+        
+
+        // ADD_NEW_METHODS_HERE
+
+    signals:
+        void signalReturnPressed();
+        
+    protected:
+        void keyPressEvent(QKeyEvent* event) override;
+        
+    private:
+        // ADD_NEW_MEMBERS_HERE
+
+    };
+    
+#ifdef __WU_Q_SPIN_BOX_DECLARE__
+    // <PLACE DECLARATIONS OF STATIC MEMBERS HERE>
+#endif // __WU_Q_SPIN_BOX_DECLARE__
+
+} // namespace
+#endif  //__WU_Q_SPIN_BOX_H__
