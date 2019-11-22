d00440b3a7c910377eb65f63fb0f3cc5d194a3fd
John Harwell <jharwell@wustl.edu>
2019-11-22 15:26:17 -0600

    Toolbar: Add Preference for Window ToolBar Width Mode.  "Compact" is normal toolbar width mode (View widgets hidden in Annotation and Tile Layout Modes).  Elongated always displays the View Mode widgets but requires a wide display (1920 or greater) to avoid clipping of the toolbar.

---
+++ src/Common/ToolBarWidthModeEnum.cxx
@@ -0,0 +1,375 @@
+
+/*LICENSE_START*/
+/*
+ *  Copyright (C) 2019 Washington University School of Medicine
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+/*LICENSE_END*/
+
+#include <algorithm>
+#define __TOOL_BAR_WIDTH_MODE_ENUM_DECLARE__
+#include "ToolBarWidthModeEnum.h"
+#undef __TOOL_BAR_WIDTH_MODE_ENUM_DECLARE__
+
+#include "CaretAssert.h"
+
+using namespace caret;
+
+    
+/**
+ * \class caret::ToolBarWidthModeEnum 
+ * \brief <REPLACE-WITH-ONE-LINE-DESCRIPTION>
+ *
+ * <REPLACE-WITH-THOROUGH DESCRIPTION>
+ *
+ * Using this enumerated type in the GUI with an EnumComboBoxTemplate
+ * 
+ * Header File (.h)
+ *     Forward declare the data type:
+ *         class EnumComboBoxTemplate;
+ * 
+ *     Declare the member:
+ *         EnumComboBoxTemplate* m_toolBarWidthModeEnumComboBox;
+ * 
+ *     Declare a slot that is called when user changes selection
+ *         private slots:
+ *             void toolBarWidthModeEnumComboBoxItemActivated();
+ * 
+ * Implementation File (.cxx)
+ *     Include the header files
+ *         #include "EnumComboBoxTemplate.h"
+ *         #include "ToolBarWidthModeEnum.h"
+ * 
+ *     Instatiate:
+ *         m_toolBarWidthModeEnumComboBox = new EnumComboBoxTemplate(this);
+ *         m_toolBarWidthModeEnumComboBox->setup<ToolBarWidthModeEnum,ToolBarWidthModeEnum::Enum>();
+ * 
+ *     Get notified when the user changes the selection: 
+ *         QObject::connect(m_toolBarWidthModeEnumComboBox, SIGNAL(itemActivated()),
+ *                          this, SLOT(toolBarWidthModeEnumComboBoxItemActivated()));
+ * 
+ *     Update the selection:
+ *         m_toolBarWidthModeEnumComboBox->setSelectedItem<ToolBarWidthModeEnum,ToolBarWidthModeEnum::Enum>(NEW_VALUE);
+ * 
+ *     Read the selection:
+ *         const ToolBarWidthModeEnum::Enum VARIABLE = m_toolBarWidthModeEnumComboBox->getSelectedItem<ToolBarWidthModeEnum,ToolBarWidthModeEnum::Enum>();
+ * 
+ */
+
+/**
+ * Constructor.
+ *
+ * @param enumValue
+ *    An enumerated value.
+ * @param name
+ *    Name of enumerated value.
+ *
+ * @param guiName
+ *    User-friendly name for use in user-interface.
+ */
+ToolBarWidthModeEnum::ToolBarWidthModeEnum(const Enum enumValue,
+                           const AString& name,
+                           const AString& guiName)
+{
+    this->enumValue = enumValue;
+    this->integerCode = integerCodeCounter++;
+    this->name = name;
+    this->guiName = guiName;
+}
+
+/**
+ * Destructor.
+ */
+ToolBarWidthModeEnum::~ToolBarWidthModeEnum()
+{
+}
+
+/**
+ * Initialize the enumerated metadata.
+ */
+void
+ToolBarWidthModeEnum::initialize()
+{
+    if (initializedFlag) {
+        return;
+    }
+    initializedFlag = true;
+
+    enumData.push_back(ToolBarWidthModeEnum(COMPACT, 
+                                    "COMPACT", 
+                                    "Compact"));
+    
+    enumData.push_back(ToolBarWidthModeEnum(ELONGATED, 
+                                    "ELONGATED", 
+                                    "Elongated"));
+    
+}
+
+/**
+ * Find the data for and enumerated value.
+ * @param enumValue
+ *     The enumerated value.
+ * @return Pointer to data for this enumerated type
+ * or NULL if no data for type or if type is invalid.
+ */
+const ToolBarWidthModeEnum*
+ToolBarWidthModeEnum::findData(const Enum enumValue)
+{
+    if (initializedFlag == false) initialize();
+
+    size_t num = enumData.size();
+    for (size_t i = 0; i < num; i++) {
+        const ToolBarWidthModeEnum* d = &enumData[i];
+        if (d->enumValue == enumValue) {
+            return d;
+        }
+    }
+
+    return NULL;
+}
+
+/**
+ * Get a string representation of the enumerated type.
+ * @param enumValue 
+ *     Enumerated value.
+ * @return 
+ *     String representing enumerated value.
+ */
+AString 
+ToolBarWidthModeEnum::toName(Enum enumValue) {
+    if (initializedFlag == false) initialize();
+    
+    const ToolBarWidthModeEnum* enumInstance = findData(enumValue);
+    return enumInstance->name;
+}
+
+/**
+ * Get an enumerated value corresponding to its name.
+ * @param name 
+ *     Name of enumerated value.
+ * @param isValidOut 
+ *     If not NULL, it is set indicating that a
+ *     enum value exists for the input name.
+ * @return 
+ *     Enumerated value.
+ */
+ToolBarWidthModeEnum::Enum 
+ToolBarWidthModeEnum::fromName(const AString& name, bool* isValidOut)
+{
+    if (initializedFlag == false) initialize();
+    
+    bool validFlag = false;
+    Enum enumValue = ToolBarWidthModeEnum::enumData[0].enumValue;
+    
+    for (std::vector<ToolBarWidthModeEnum>::iterator iter = enumData.begin();
+         iter != enumData.end();
+         iter++) {
+        const ToolBarWidthModeEnum& d = *iter;
+        if (d.name == name) {
+            enumValue = d.enumValue;
+            validFlag = true;
+            break;
+        }
+    }
+    
+    if (isValidOut != 0) {
+        *isValidOut = validFlag;
+    }
+    else if (validFlag == false) {
+        CaretAssertMessage(0, AString("Name " + name + "failed to match enumerated value for type ToolBarWidthModeEnum"));
+    }
+    return enumValue;
+}
+
+/**
+ * Get a GUI string representation of the enumerated type.
+ * @param enumValue 
+ *     Enumerated value.
+ * @return 
+ *     String representing enumerated value.
+ */
+AString 
+ToolBarWidthModeEnum::toGuiName(Enum enumValue) {
+    if (initializedFlag == false) initialize();
+    
+    const ToolBarWidthModeEnum* enumInstance = findData(enumValue);
+    return enumInstance->guiName;
+}
+
+/**
+ * Get an enumerated value corresponding to its GUI name.
+ * @param s 
+ *     Name of enumerated value.
+ * @param isValidOut 
+ *     If not NULL, it is set indicating that a
+ *     enum value exists for the input name.
+ * @return 
+ *     Enumerated value.
+ */
+ToolBarWidthModeEnum::Enum 
+ToolBarWidthModeEnum::fromGuiName(const AString& guiName, bool* isValidOut)
+{
+    if (initializedFlag == false) initialize();
+    
+    bool validFlag = false;
+    Enum enumValue = ToolBarWidthModeEnum::enumData[0].enumValue;
+    
+    for (std::vector<ToolBarWidthModeEnum>::iterator iter = enumData.begin();
+         iter != enumData.end();
+         iter++) {
+        const ToolBarWidthModeEnum& d = *iter;
+        if (d.guiName == guiName) {
+            enumValue = d.enumValue;
+            validFlag = true;
+            break;
+        }
+    }
+    
+    if (isValidOut != 0) {
+        *isValidOut = validFlag;
+    }
+    else if (validFlag == false) {
+        CaretAssertMessage(0, AString("guiName " + guiName + "failed to match enumerated value for type ToolBarWidthModeEnum"));
+    }
+    return enumValue;
+}
+
+/**
+ * Get the integer code for a data type.
+ *
+ * @return
+ *    Integer code for data type.
+ */
+int32_t
+ToolBarWidthModeEnum::toIntegerCode(Enum enumValue)
+{
+    if (initializedFlag == false) initialize();
+    const ToolBarWidthModeEnum* enumInstance = findData(enumValue);
+    return enumInstance->integerCode;
+}
+
+/**
+ * Find the data type corresponding to an integer code.
+ *
+ * @param integerCode
+ *     Integer code for enum.
+ * @param isValidOut
+ *     If not NULL, on exit isValidOut will indicate if
+ *     integer code is valid.
+ * @return
+ *     Enum for integer code.
+ */
+ToolBarWidthModeEnum::Enum
+ToolBarWidthModeEnum::fromIntegerCode(const int32_t integerCode, bool* isValidOut)
+{
+    if (initializedFlag == false) initialize();
+    
+    bool validFlag = false;
+    Enum enumValue = ToolBarWidthModeEnum::enumData[0].enumValue;
+    
+    for (std::vector<ToolBarWidthModeEnum>::iterator iter = enumData.begin();
+         iter != enumData.end();
+         iter++) {
+        const ToolBarWidthModeEnum& enumInstance = *iter;
+        if (enumInstance.integerCode == integerCode) {
+            enumValue = enumInstance.enumValue;
+            validFlag = true;
+            break;
+        }
+    }
+    
+    if (isValidOut != 0) {
+        *isValidOut = validFlag;
+    }
+    else if (validFlag == false) {
+        CaretAssertMessage(0, AString("Integer code " + AString::number(integerCode) + "failed to match enumerated value for type ToolBarWidthModeEnum"));
+    }
+    return enumValue;
+}
+
+/**
+ * Get all of the enumerated type values.  The values can be used
+ * as parameters to toXXX() methods to get associated metadata.
+ *
+ * @param allEnums
+ *     A vector that is OUTPUT containing all of the enumerated values.
+ */
+void
+ToolBarWidthModeEnum::getAllEnums(std::vector<ToolBarWidthModeEnum::Enum>& allEnums)
+{
+    if (initializedFlag == false) initialize();
+    
+    allEnums.clear();
+    
+    for (std::vector<ToolBarWidthModeEnum>::iterator iter = enumData.begin();
+         iter != enumData.end();
+         iter++) {
+        allEnums.push_back(iter->enumValue);
+    }
+}
+
+/**
+ * Get all of the names of the enumerated type values.
+ *
+ * @param allNames
+ *     A vector that is OUTPUT containing all of the names of the enumerated values.
+ * @param isSorted
+ *     If true, the names are sorted in alphabetical order.
+ */
+void
+ToolBarWidthModeEnum::getAllNames(std::vector<AString>& allNames, const bool isSorted)
+{
+    if (initializedFlag == false) initialize();
+    
+    allNames.clear();
+    
+    for (std::vector<ToolBarWidthModeEnum>::iterator iter = enumData.begin();
+         iter != enumData.end();
+         iter++) {
+        allNames.push_back(ToolBarWidthModeEnum::toName(iter->enumValue));
+    }
+    
+    if (isSorted) {
+        std::sort(allNames.begin(), allNames.end());
+    }
+}
+
+/**
+ * Get all of the GUI names of the enumerated type values.
+ *
+ * @param allNames
+ *     A vector that is OUTPUT containing all of the GUI names of the enumerated values.
+ * @param isSorted
+ *     If true, the names are sorted in alphabetical order.
+ */
+void
+ToolBarWidthModeEnum::getAllGuiNames(std::vector<AString>& allGuiNames, const bool isSorted)
+{
+    if (initializedFlag == false) initialize();
+    
+    allGuiNames.clear();
+    
+    for (std::vector<ToolBarWidthModeEnum>::iterator iter = enumData.begin();
+         iter != enumData.end();
+         iter++) {
+        allGuiNames.push_back(ToolBarWidthModeEnum::toGuiName(iter->enumValue));
+    }
+    
+    if (isSorted) {
+        std::sort(allGuiNames.begin(), allGuiNames.end());
+    }
+}
+
