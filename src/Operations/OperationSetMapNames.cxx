cec7ba854f15944eddf5799ba9756769ea22a513
Tim Coalson <tsc5yc@mst.edu>
2020-01-23 18:40:42 -0600

    add -from-data-file option to -set-map-names

--- src/Operations/OperationSetMapNames.cxx
+++ src/Operations/OperationSetMapNames.cxx
@@ -50,14 +50,17 @@ OperationParameters* OperationSetMapNames::getParameters()
     OptionalParameter* fileOpt = ret->createOptionalParameter(2, "-name-file", "use a text file to replace all map names");
     fileOpt->addStringParameter(1, "file", "text file containing map names, one per line");
     
+    OptionalParameter* dataFileOpt = ret->createOptionalParameter(4, "-from-data-file", "use the map names from another data file");
+    dataFileOpt->addStringParameter(1, "file", "a data file with the same number of maps");
+    
     ParameterComponent* mapOpt = ret->createRepeatableParameter(3, "-map", "specify a map to set the name of");
     mapOpt->addIntegerParameter(1, "index", "the map index to change the name of");
     mapOpt->addStringParameter(2, "new-name", "the name to set for the map");
     
     ret->setHelpText(
         AString("Sets the name of one or more maps for metric, shape, label, volume, cifti scalar or cifti label files.  ") +
-        "If the -name-file option is not specified, the -map option must be specified at least once.  " +
-        "The -map option cannot be used when -name-file is specified."
+        "You must specify either -name-file, or -from-data-file, or at least one -map option.  " +
+        "The three option types are mutually exclusive."
     );
     return ret;
 }
@@ -67,6 +70,7 @@ void OperationSetMapNames::useParameters(OperationParameters* myParams, Progress
     LevelProgress myProgress(myProgObj);
     AString fileName = myParams->getString(1);
     OptionalParameter* fileOpt = myParams->getOptionalParameter(2);
+    OptionalParameter* dataFileOpt = myParams->getOptionalParameter(4);
     const vector<ParameterComponent*>& mapOpts = *(myParams->getRepeatableParameterInstances(3));
     CaretPointer<CaretDataFile> caretDataFile(CaretDataFileHelper::readAnyCaretDataFile(fileName));
     CaretMappableDataFile* mappableFile = dynamic_cast<CaretMappableDataFile*>(caretDataFile.getPointer());
@@ -74,6 +78,7 @@ void OperationSetMapNames::useParameters(OperationParameters* myParams, Progress
     if (fileOpt->m_present)
     {
         if (!mapOpts.empty()) throw OperationException("-map may not be specified when using -name-file");
+        if (dataFileOpt->m_present) throw OperationException("-from-data-file may not be specified when using -name-file");
         AString listfileName = fileOpt->getString(1);
         ifstream nameListFile(listfileName.toLocal8Bit().constData());
         if (!nameListFile.good())
@@ -101,14 +106,27 @@ void OperationSetMapNames::useParameters(OperationParameters* myParams, Progress
             throw OperationException("name file contains more names than can be used on the file");
         }
     } else {
-        if (mapOpts.empty()) throw OperationException("you must specify at least one option that sets a map name");
-        for (int i = 0; i < (int)mapOpts.size(); ++i)
-        {
-            int mapIndex = (int)mapOpts[i]->getInteger(1) - 1;
-            if (mapIndex < 0) throw OperationException("invalid map index, indices are 1-based");
-            if (mapIndex >= mappableFile->getNumberOfMaps()) throw OperationException("invalid map index, file doesn't have enough maps");
-            AString newName = mapOpts[i]->getString(2);
-            mappableFile->setMapName(mapIndex, newName);
+        if (dataFileOpt->m_present) {
+            if (!mapOpts.empty()) throw OperationException("-map may not be specified when using -from-data-file");
+            CaretPointer<CaretDataFile> nameCaretDataFile(CaretDataFileHelper::readAnyCaretDataFile(dataFileOpt->getString(1)));
+            CaretMappableDataFile* nameMappableFile = dynamic_cast<CaretMappableDataFile*>(nameCaretDataFile.getPointer());
+            if (nameMappableFile == NULL) throw OperationException("cannot read map names from this file type");
+            int numMaps = mappableFile->getNumberOfMaps();
+            if (nameMappableFile->getNumberOfMaps() != numMaps) throw OperationException("-from-data-file specifies a file with a different number of maps than the file to modify");
+            for (int i = 0; i < numMaps; ++i)
+            {
+                mappableFile->setMapName(i, nameMappableFile->getMapName(i));//NOTE: getMapName can give #1, #2, etc when map names are actually blank
+            }
+        } else {
+            if (mapOpts.empty()) throw OperationException("you must specify at least one option that sets a map name");
+            for (int i = 0; i < (int)mapOpts.size(); ++i)
+            {
+                int mapIndex = (int)mapOpts[i]->getInteger(1) - 1;
+                if (mapIndex < 0) throw OperationException("invalid map index, indices are 1-based");
+                if (mapIndex >= mappableFile->getNumberOfMaps()) throw OperationException("invalid map index, file doesn't have enough maps");
+                AString newName = mapOpts[i]->getString(2);
+                mappableFile->setMapName(mapIndex, newName);
+            }
         }
     }
     mappableFile->writeFile(fileName);
