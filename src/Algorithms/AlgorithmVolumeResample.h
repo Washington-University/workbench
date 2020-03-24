38d2333b4400a662eee4fa6f73bf76a2d849805f
Tim Coalson <tsc5yc@mst.edu>
2020-03-23 21:22:39 -0500

    fix copyright year

--- src/Algorithms/AlgorithmVolumeResample.h
+++ src/Algorithms/AlgorithmVolumeResample.h
@@ -3,7 +3,7 @@
 
 /*LICENSE_START*/
 /*
- *  Copyright (C) 2018  Washington University School of Medicine
+ *  Copyright (C) 2020  Washington University School of Medicine
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
