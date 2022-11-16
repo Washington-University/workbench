5a8acef1d1c7c277556037bb5702a5034a7387ee
Tim Coalson <coalsont@wustl.edu>
2022-11-15 18:40:11 -0600

    add MIT exceptions for FSL/MSM to octtree-related code

--- src/Common/OctTree.h
+++ src/Common/OctTree.h
@@ -21,6 +21,32 @@
  */
 /*LICENSE_END*/
 
+/*This code can be used with the Oxford FMRIB Software Library and/or
+ * Multimodal Surface Matching under the MIT license instead of the
+ * above GPL license.  The MIT license follows:*/
+/*
+ *  Copyright (C) Washington University School of Medicine
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining
+ *  a copy of this software and associated documentation files (the
+ *  "Software"), to deal in the Software without restriction, including
+ *  without limitation the rights to use, copy, modify, merge, publish,
+ *  distribute, sublicense, and/or sell copies of the Software, and to
+ *  permit persons to whom the Software is furnished to do so, subject to
+ *  the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ *  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ *  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #include "MathFunctions.h"
 #include <vector>
 
