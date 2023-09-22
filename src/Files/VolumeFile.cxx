c13905b86b054f0ed45071bf180aec394e64202b
Tim Coalson <coalsont@wustl.edu>
2023-09-21 19:45:40 -0500

    Fix crash from VolumeFile::getType accessing a palette without checking if the file is label-type

--- src/Files/VolumeFile.cxx
+++ src/Files/VolumeFile.cxx
@@ -182,15 +182,14 @@ void VolumeFile::addSubvolumes(const int64_t& numToAdd)
 
 SubvolumeAttributes::VolumeType VolumeFile::getType() const
 {
-    if (getNumberOfMaps() >= 3) {
-        const int32_t mapIndex(0);
-        if (getMapPaletteColorMapping(mapIndex)->getSelectedPaletteName() == Palette::SPECIAL_RGB_VOLUME_PALETTE_NAME) {
+    CaretAssertVectorIndex(m_caretVolExt.m_attributes, 0);
+    CaretAssert(m_caretVolExt.m_attributes[0] != NULL);
+    //it is an error to get the palette for a label-type volume
+    if (!isMappedWithLabelTable() && getNumberOfMaps() >= 3) {
+        if (getMapPaletteColorMapping(0)->getSelectedPaletteName() == Palette::SPECIAL_RGB_VOLUME_PALETTE_NAME) {
             return SubvolumeAttributes::RGB_WORKBENCH;
         }
     }
-
-    CaretAssertVectorIndex(m_caretVolExt.m_attributes, 0);
-    CaretAssert(m_caretVolExt.m_attributes[0] != NULL);
     return m_caretVolExt.m_attributes[0]->m_type;
 }
 
