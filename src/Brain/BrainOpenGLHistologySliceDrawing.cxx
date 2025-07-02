6a8bb79965343707de6c2021c76cbb1de2be1a24
John Harwell <jharwell@wustl.edu>
2025-07-02 10:37:43 -0500

    Backing out histology rotation commits (2/4) Revert "Fixed crash of histology drawing when histology slice aspect ratio was greater than the viewport's aspect ratio."
    This reverts commit 91aa35d30de652f1d5500afe4d022ead0b2eba5a.

--- src/Brain/BrainOpenGLHistologySliceDrawing.cxx
+++ src/Brain/BrainOpenGLHistologySliceDrawing.cxx
@@ -158,8 +158,6 @@ BrainOpenGLHistologySliceDrawing::getOrthoBounds(GraphicsOrthographicProjection&
     const double marginPercent(0.02);
     const double marginSizePixels = imageHeight * marginPercent;
     if (imageAspectRatio > viewportAspectRatio) {
-        orthoBottomOut = boundingBox.getMinY() - marginSizePixels;
-        orthoTopOut    = boundingBox.getMaxY() + marginSizePixels;
         if (originTopLeftFlag) {
             std::swap(orthoBottomOut, orthoTopOut);
         }
@@ -333,6 +331,7 @@ BrainOpenGLHistologySliceDrawing::draw(BrainOpenGLFixedPipeline* fixedPipelineDr
     if ( ! getOrthoBounds(orthographicProjection)) {
         return;
     }
+    std::cout << "Ortho: " << orthographicProjection.toString() << std::endl;
 
     setupScaleBars(orthographicProjection);
     
