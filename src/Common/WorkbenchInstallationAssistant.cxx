a74aa2021288fef0ec21aac133454ca0834d7cf0
Tim Coalson <coalsont@wustl.edu>
2025-07-31 17:21:09 -0500

    attempted general fix for qEnvironment include

--- src/Common/WorkbenchInstallationAssistant.cxx
+++ src/Common/WorkbenchInstallationAssistant.cxx
@@ -25,7 +25,7 @@
 
 #include <QDir>
 #include <QFileInfo>
-#if QT_VERSION >= QT_VERSION_CHECK(5, 13, 0)
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
 #include <QtEnvironmentVariables>
 #endif
 #include <QtGlobal>
