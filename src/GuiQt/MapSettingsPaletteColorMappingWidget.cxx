29ba341c5907228e5f80f70de6afcf53a33ef7d1
John Harwell <jharwell@wustl.edu>
2026-04-28 11:13:46 -0500

    Added removal of event listeners

--- src/GuiQt/MapSettingsPaletteColorMappingWidget.cxx
+++ src/GuiQt/MapSettingsPaletteColorMappingWidget.cxx
@@ -184,6 +184,7 @@ MapSettingsPaletteColorMappingWidget::MapSettingsPaletteColorMappingWidget(QWidg
  */
 MapSettingsPaletteColorMappingWidget::~MapSettingsPaletteColorMappingWidget()
 {
+    EventManager::get()->removeAllEventsFromListener(this);
 }
 
 /**
